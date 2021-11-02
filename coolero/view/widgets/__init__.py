4f71aee2e41e43c8f8edf92a4378bf0fefbc0115
Guy Boldon <gb@guyboldon.com>
2021-11-02 20:36:33 +0100

    widgets - module imports

---
+++ coolero/view/widgets/__init__.py
@@ -0,0 +1,26 @@
+#  Coolero - monitor and control your cooling and other devices
+#  Copyright (c) 2021  Guy Boldon
+#  All credit for basis of the user interface (GUI) goes to: Wanderson M.Pimenta
+#  |
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  |
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  |
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------------------------------------------------
+
+from .py_credits_bar import PyCredits
+from .py_grips import PyGrips
+from .py_left_column import PyLeftColumn
+from .py_left_menu import PyLeftMenu
+from .py_push_button import PyPushButton
+from .py_title_bar import PyTitleBar
+from .py_toggle import PyToggle
+from .py_window import PyWindow
