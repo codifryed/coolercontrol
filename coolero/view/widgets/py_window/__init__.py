6480153804ed7fec1398a9cc249d22723a4481cf
Guy Boldon <gb@guyboldon.com>
2021-11-02 20:07:41 +0100

    window widget

---
+++ coolero/view/widgets/py_window/__init__.py
@@ -0,0 +1,19 @@
+#  Coolero - monitor and control your cooling and other devices
+#  Copyright (c) 2021  Guy Boldon
+#  All credit for basis of the user interface (GUI) goes to: Wanderson M.Pimenta
+#  |
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  |
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  |
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------------------------------------------------
+
+from .py_window import PyWindow
---
+++ coolero/view/widgets/py_window/styles.py
@@ -0,0 +1,30 @@
+#  Coolero - monitor and control your cooling and other devices
+#  Copyright (c) 2021  Guy Boldon
+#  All credit for basis of the user interface (GUI) goes to: Wanderson M.Pimenta
+#  |
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  |
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  |
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------------------------------------------------
+
+class Styles:
+    bg_style = """
+    #pod_bg_app {{
+        background-color: {_bg_color};
+        border-radius: {_border_radius};
+        border: {_border_size}px solid {_border_color};
+    }}
+    QFrame {{ 
+        color: {_text_color};
+        font: {_text_font};
+    }}
+    """
