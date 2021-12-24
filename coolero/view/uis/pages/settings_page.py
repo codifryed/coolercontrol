b50df75ff31bf8d5ea5adc44f323660f6fe4ab77
Guy Boldon <gb@guyboldon.com>
2021-12-24 14:34:48 +0100

    improve settings page layout for different settings types

--- coolero/view/uis/pages/settings_page.py
+++ coolero/view/uis/pages/settings_page.py
@@ -30,18 +30,8 @@ class SettingsPage(QWidget):
         super().__init__()
         # layout
         self.theme: Dict = Settings.theme
-        self.base_layout = QVBoxLayout(self)
-        self.main_layout = QHBoxLayout()
-        self.main_layout.setAlignment(Qt.AlignTop)
         self.setStyleSheet('font: 14pt')
-        self.label_layout = QVBoxLayout()
-        self.main_layout.addLayout(self.label_layout)
-        self.switch_layout = QVBoxLayout()
-        self.main_layout.addLayout(self.switch_layout)
-        self.base_layout.addLayout(self.main_layout)
-        self.notes_layout = QVBoxLayout()
-        self.notes_layout.setAlignment(Qt.AlignBottom)
-        self.base_layout.addLayout(self.notes_layout)
+        self.base_layout = QVBoxLayout(self)
         self.toggle_bg_color = self.theme["app_color"]["dark_two"]
         self.toggle_circle_color = self.theme["app_color"]["icon_color"]
         self.toggle_active_color = self.theme["app_color"]["context_color"]
@@ -50,16 +40,20 @@ class SettingsPage(QWidget):
         self.setting_enable_light_theme()
         self.setting_hide_on_close()
 
+        self.notes_layout = QVBoxLayout()
+        self.notes_layout.setAlignment(Qt.AlignBottom)
         self.requires_restart_label = QLabel()
         self.requires_restart_label.setTextFormat(Qt.TextFormat.RichText)
         self.requires_restart_label.setText('<i>*requires restart</i>')
         self.requires_restart_label.setAlignment(Qt.AlignRight)
         self.notes_layout.addWidget(self.requires_restart_label)
+        self.base_layout.addLayout(self.notes_layout)
 
     def setting_save_window_size(self) -> None:
+        save_window_size_layout = QHBoxLayout()
         save_window_size_label = QLabel()
         save_window_size_label.setText("Save Window Size on Exit")
-        self.label_layout.addWidget(save_window_size_label)
+        save_window_size_layout.addWidget(save_window_size_label)
         save_window_size_toggle = PyToggle(
             bg_color=self.toggle_bg_color,
             circle_color=self.toggle_circle_color,
@@ -68,12 +62,14 @@ class SettingsPage(QWidget):
         )
         save_window_size_toggle.setObjectName(UserSettings.SAVE_WINDOW_SIZE)
         save_window_size_toggle.clicked.connect(self.setting_toggled)
-        self.switch_layout.addWidget(save_window_size_toggle)
+        save_window_size_layout.addWidget(save_window_size_toggle)
+        self.base_layout.addLayout(save_window_size_layout)
 
     def setting_enable_light_theme(self) -> None:
+        enable_light_theme_layout = QHBoxLayout()
         enable_light_theme_label = QLabel()
         enable_light_theme_label.setText('Enable Light Theme*')
-        self.label_layout.addWidget(enable_light_theme_label)
+        enable_light_theme_layout.addWidget(enable_light_theme_label)
         enable_light_theme_toggle = PyToggle(
             bg_color=self.toggle_bg_color,
             circle_color=self.toggle_circle_color,
@@ -82,12 +78,14 @@ class SettingsPage(QWidget):
         )
         enable_light_theme_toggle.setObjectName(UserSettings.ENABLE_LIGHT_THEME)
         enable_light_theme_toggle.clicked.connect(self.setting_toggled)
-        self.switch_layout.addWidget(enable_light_theme_toggle)
+        enable_light_theme_layout.addWidget(enable_light_theme_toggle)
+        self.base_layout.addLayout(enable_light_theme_layout)
 
     def setting_hide_on_close(self) -> None:
+        hide_on_close_layout = QHBoxLayout()
         hide_on_close_label = QLabel()
         hide_on_close_label.setText('Minimize To Tray on Close')
-        self.label_layout.addWidget(hide_on_close_label)
+        hide_on_close_layout.addWidget(hide_on_close_label)
         hide_on_close_toggle = PyToggle(
             bg_color=self.toggle_bg_color,
             circle_color=self.toggle_circle_color,
@@ -96,7 +94,8 @@ class SettingsPage(QWidget):
         )
         hide_on_close_toggle.setObjectName(UserSettings.HIDE_ON_CLOSE)
         hide_on_close_toggle.clicked.connect(self.setting_toggled)
-        self.switch_layout.addWidget(hide_on_close_toggle)
+        hide_on_close_layout.addWidget(hide_on_close_toggle)
+        self.base_layout.addLayout(hide_on_close_layout)
 
     @Slot(bool)  # type: ignore[operator]
     def setting_toggled(self, checked: bool) -> None:
