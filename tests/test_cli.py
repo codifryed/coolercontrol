97691bd67d1af9fdceb33841bc6c0356f1a463d6
Guy Boldon <gb@guyboldon.com>
2022-02-26 17:29:54 +0100

    add script runner for app

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,12 +3,12 @@ from subprocess import check_output
 
 
 def test_show_help() -> None:
-    output: str = check_output(["python3", "coolero/coolero.py", "-h"], encoding='UTF-8')
+    output: str = check_output(["python3", "coolero.py", "-h"], encoding='UTF-8')
     assert "coolero" in output
     assert "help" in output
 
 
 def test_show_version() -> None:
     pattern = re.compile(r'.*Coolero v\d+\.\d+\.\d+.*')
-    output: str = check_output(["python3", "coolero/coolero.py", "-v"], encoding='UTF-8')
+    output: str = check_output(["python3", "coolero.py", "-v"], encoding='UTF-8')
     assert pattern.search(output)
