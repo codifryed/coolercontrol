c1402d1f67b751a1348be79e72cf723c31d3839c
Guy Boldon <gb@guyboldon.com>
2022-04-03 23:26:25 +0200

    show detected hwmon devices on info page

--- coolero/view/uis/pages/info_page.py
+++ coolero/view/uis/pages/info_page.py
@@ -87,6 +87,7 @@ class InfoPage(QScrollArea):
         cpu_text = ''
         gpu_text = ''
         lc_text = ''
+        hwmon_text = ''
         if not devices:
             lc_text = '<h4>None</h4>'
         else:
@@ -97,7 +98,9 @@ class InfoPage(QScrollArea):
                     gpu_text += f'<h4>GPU #{device.type_id}</h4>{device.name}<br>'
                 if device.type == DeviceType.LIQUIDCTL:
                     lc_text += f'<h4>Liquidctl device #{device.type_id}</h4>{device.name}<br>'
-        label.setText(detected_devices + cpu_text + gpu_text + lc_text)
+                if device.type == DeviceType.HWMON:
+                    hwmon_text += f'<h4>Hwmon device #{device.type_id}</h4>{device.name}<br>'
+        label.setText(detected_devices + cpu_text + gpu_text + lc_text + hwmon_text)
         self._base_layout.addWidget(label)
 
     def _debug_text(self) -> None:
