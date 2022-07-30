1367a21b591eeabb19bd04791eda4eb0805b72ae
Guy Boldon <gb@guyboldon.com>
2022-07-30 14:08:01 +0200

    add clap and parse debug cli option

--- coolercontrol/src/main.rs
+++ coolercontrol/src/main.rs
@@ -1,3 +1,33 @@
+/*
+ * Coolero - monitor and control your cooling and other devices
+ * Copyright (c) 2022  Guy Boldon
+ * |
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * |
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * |
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(long)]
+    debug: bool,
+}
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
+    println!("Debug: {}", args.debug)
 }
