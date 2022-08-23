2a7e0ea1a8ae1a86bce1d903f5a0900aa1f2bd24
Guy Boldon <gb@guyboldon.com>
2022-08-23 14:32:33 +0200

    remove unnecessary info log

--- coolercontrol/src/liquidctl/liqctld_client.rs
+++ coolercontrol/src/liquidctl/liqctld_client.rs
@@ -170,7 +170,6 @@ impl Client {
         if !response.error.is_empty() {
             Err(anyhow!("Error trying to initialize devices: {}", response.error))
         } else {
-            info!("Liquidctl device initialization complete");
             Ok(response.success)
         }
     }
