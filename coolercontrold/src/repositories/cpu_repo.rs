39352788f675de7ba1fdf84feb31475497ea3b46
Guy Boldon <gb@guyboldon.com>
2023-03-04 17:34:13 +0100

    daemon: preload status for cpu devices

--- coolercontrold/src/repositories/cpu_repo.rs
+++ coolercontrold/src/repositories/cpu_repo.rs
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  ******************************************************************************/
 
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
@@ -26,6 +27,7 @@ use psutil::sensors::TemperatureSensor;
 use tokio::process::Command;
 use tokio::sync::RwLock;
 use tokio::time::Instant;
+use zbus::export::futures_util::future::join_all;
 
 use crate::device::{ChannelStatus, Device, DeviceInfo, DeviceType, Status, TempStatus, UID};
 use crate::repositories::repository::{DeviceList, Repository};
@@ -44,6 +46,7 @@ pub struct CpuRepo {
     cpu_collector: RwLock<CpuPercentCollector>,
     current_sensor_name: RwLock<Option<String>>,
     current_label_name: RwLock<Option<String>>,
+    preloaded_statuses: RwLock<HashMap<u8, (Vec<ChannelStatus>, Vec<TempStatus>)>>,
 }
 
 impl CpuRepo {
@@ -53,10 +56,11 @@ impl CpuRepo {
             cpu_collector: RwLock::new(CpuPercentCollector::new()?),
             current_sensor_name: RwLock::new(None),
             current_label_name: RwLock::new(None),
+            preloaded_statuses: RwLock::new(HashMap::new()),
         })
     }
 
-    async fn request_status(&self) -> Result<Status> {
+    async fn request_status(&self) -> Result<(Vec<ChannelStatus>, Vec<TempStatus>)> {
         let mut temp_sensors = vec![];
         for sensor_result in psutil::sensors::temperatures() {
             if let Ok(sensor) = sensor_result {
@@ -68,15 +72,14 @@ impl CpuRepo {
             // only log all responses the first time
             debug!("Detected temperature sensors: {:?}", temp_sensors);
         }
-        // todo: request_status_known(temp_sensors) if current_* is set -> for small speedup
-        self.request_status_new(temp_sensors).await
+        self.request_statuses_new(temp_sensors).await
     }
 
     /// This is used to find the correct sensors and labels for cpu data.
-    async fn request_status_new(
+    async fn request_statuses_new(
         &self,
         temp_sensors: Vec<TemperatureSensor>,
-    ) -> Result<Status> {
+    ) -> Result<(Vec<ChannelStatus>, Vec<TempStatus>)> {
         for cpu_sensor_name in PSUTIL_CPU_SENSOR_NAMES {  // order is important
             for temp_sensor in &temp_sensors {
                 if temp_sensor.unit() == cpu_sensor_name {
@@ -86,21 +89,20 @@ impl CpuRepo {
                             if label.contains(cpu_label) {
                                 self.set_current_sensor_names(cpu_sensor_name, &label).await;
                                 let cpu_usage = self.cpu_collector.write().await.cpu_percent()?;
-                                return Ok(Status {
-                                    temps: vec![TempStatus {
-                                        name: CPU_TEMP_NAME.to_string(),
-                                        temp: temp_sensor.current().celsius(),
-                                        frontend_name: CPU_TEMP_NAME.to_string(),
-                                        external_name: CPU_TEMP_NAME.to_string(),
-                                    }],
-                                    channels: vec![ChannelStatus {
+                                return Ok((
+                                    vec![ChannelStatus {
                                         name: CPU_LOAD_NAME.to_string(),
                                         rpm: None,
                                         duty: Some(cpu_usage as f64),
                                         pwm_mode: None,
                                     }],
-                                    ..Default::default()
-                                });
+                                    vec![TempStatus {
+                                        name: CPU_TEMP_NAME.to_string(),
+                                        temp: temp_sensor.current().celsius(),
+                                        frontend_name: CPU_TEMP_NAME.to_string(),
+                                        external_name: CPU_TEMP_NAME.to_string(),
+                                    }],
+                                ));
                             }
                         }
                     }
@@ -162,13 +164,21 @@ impl Repository for CpuRepo {
         //   to really properly track wich cpu socket belongs to which temp we need to handle
         //   the hwmon files ourselves. (device path aka UID)
         debug!("Starting Device Initialization");
+        let type_index = 1u8;
         let start_initialization = Instant::now();
-        let status = self.request_status().await?;
+        let (channels, temps) = self.request_status().await?;
+        self.preloaded_statuses.write().await
+            .insert(type_index, (channels.clone(), temps.clone()));
+        let status = Status {
+            channels,
+            temps,
+            ..Default::default()
+        };
         let cpu_name = self.get_cpu_name().await;
         let device = Device::new(
             cpu_name,
             DeviceType::CPU,
-            1,
+            type_index,
             None,
             Some(DeviceInfo {
                 temp_max: 100,
@@ -199,12 +209,47 @@ impl Repository for CpuRepo {
         self.devices.iter().cloned().collect()
     }
 
+    async fn preload_statuses(&self) {
+        debug!("Preloading all CPU device statuses");
+        let start_update = Instant::now();
+        let mut futures = Vec::new();
+        for device_lock in &self.devices {
+            futures.push(
+                async {
+                    let status = self.request_status().await;
+                    if let Err(err) = status {
+                        error!("Error getting CPU status: {}", err);
+                        return;
+                    }
+                    let device_id = device_lock.read().await.type_index;
+                    self.preloaded_statuses.write().await.insert(device_id, status.unwrap());
+                }
+            )
+        }
+        join_all(futures).await;
+        debug!(
+            "Time taken to preload statuses for all CPU devices: {:?}",
+            start_update.elapsed()
+        );
+    }
+
     async fn update_statuses(&self) -> Result<()> {
         debug!("Updating all CPU device statuses");
         let start_update = Instant::now();
         // current only supports one device:
         for device_lock in &self.devices {
-            let status = self.request_status().await?;
+            let preloaded_statuses_map = self.preloaded_statuses.read().await;
+            let preloaded_statuses = preloaded_statuses_map.get(&device_lock.read().await.type_index);
+            if let None = preloaded_statuses {
+                error!("There is no status preloaded for this CPU device: {}", device_lock.read().await.type_index);
+                continue;
+            }
+            let (channels, temps) = preloaded_statuses.unwrap().clone();
+            let status = Status {
+                channels,
+                temps,
+                ..Default::default()
+            };
             debug!("Device status updated: {:?}", status);
             device_lock.write().await.set_status(status);
         }
