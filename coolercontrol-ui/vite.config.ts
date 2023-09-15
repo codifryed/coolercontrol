2125e9e351259a2b717fa2af1abd7e3132ba4f6a
Guy Boldon <gb@guyboldon.com>
2023-09-15 11:52:13 +0200

    ui: setup vite for single-file export

--- coolercontrol-ui/vite.config.ts
+++ coolercontrol-ui/vite.config.ts
@@ -1,16 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteSingleFile } from "vite-plugin-singlefile"
+import {viteSingleFile} from "vite-plugin-singlefile"
 import svgLoader from 'vite-svg-loader'
+import loadVersion from 'vite-plugin-package-version'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue(), viteSingleFile(), svgLoader()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [
+            vue(), viteSingleFile(), svgLoader(), loadVersion()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
 })
