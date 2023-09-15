0ca682f18addcbc29b3e092da7ac9a8f612894f5
Guy Boldon <gb@guyboldon.com>
2023-09-15 11:51:49 +0200

    ui: remove default app page

--- coolercontrol-ui/src/App.vue
+++ coolercontrol-ui/src/App.vue
@@ -4,19 +4,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-<!--  <header>-->
-<!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
-
-<!--    <div class="wrapper">-->
-<!--      <HelloWorld msg="You did it!" />-->
-
-<!--      <nav>-->
-<!--        <RouterLink to="/">Home</RouterLink>-->
-<!--        <RouterLink to="/about">About</RouterLink>-->
-<!--      </nav>-->
-<!--    </div>-->
-<!--  </header>-->
-
   <RouterView />
 </template>
 
