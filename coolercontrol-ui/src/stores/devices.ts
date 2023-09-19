4efe2ab862b727734521b43473570df674e19740
Guy Boldon <gb@guyboldon.com>
2023-09-19 17:21:42 +0200

    ui: make deviceService in the store non-reactive

--- coolercontrol-ui/src/stores/devices.ts
+++ coolercontrol-ui/src/stores/devices.ts
@@ -1,12 +1,12 @@
-import {ref, computed} from 'vue'
+import {markRaw, ref} from 'vue'
 import {defineStore} from 'pinia'
-import {DaemonService} from "@/service/DaemonService";
+import {DeviceService} from "@/service/DeviceService";
 
 /**
  * This is similar to the model_view in the old GUI, where it held global state for all the various hooks and accesses
  */
 export const useDeviceStore = defineStore('device', () => {
-    const service = ref(new DaemonService())
+    const service = markRaw(new DeviceService())
     const fullStatusUpdate = ref(false)
 
     // todo: could have a function to updateStatuses and have a variable to maintain the last sync state, whether it was for all statuses or only the most recent
