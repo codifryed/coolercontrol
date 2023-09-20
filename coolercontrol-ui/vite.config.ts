e5c337f002cfcdc63a0aa22c6ce65c5255e48bb9
Guy Boldon <gb@guyboldon.com>
2023-09-20 14:47:52 +0200

    ui: update vite and ts configs

--- coolercontrol-ui/vite.config.ts
+++ coolercontrol-ui/vite.config.ts
@@ -17,4 +17,9 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
+    build: {
+        target: 'modules',
+        minify: 'esbuild',
+        cssMinify: 'esbuild'
+    }
 })
--- coolercontrol-ui/vitest.config.ts
+++ coolercontrol-ui/vitest.config.ts
@@ -1,18 +1,19 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig } from 'vite'
-import { configDefaults, defineConfig } from 'vitest/config'
+import {fileURLToPath} from 'node:url'
+import {mergeConfig} from 'vite'
+import {configDefaults, defineConfig} from 'vitest/config'
 import viteConfig from './vite.config'
 
+/** @type {import('vite').UserConfig} */
 export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      transformMode: {
-        web: [/\.[jt]sx$/],
-      },
+    viteConfig,
+    {
+        test: {
+            environment: 'jsdom',
+            exclude: [...configDefaults.exclude, 'e2e/*'],
+            root: fileURLToPath(new URL('./', import.meta.url)),
+            transformMode: {
+                web: [/\.[jt]sx$/],
+            },
+        }
     }
-  })
 )
