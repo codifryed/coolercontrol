83acc96269238c3b5247c15440254adc7a0b292f
Guy Boldon <gb@guyboldon.com>
2023-09-22 01:42:36 +0200

    ui: create dynamic routes for device channels

--- coolercontrol-ui/src/router/index.ts
+++ coolercontrol-ui/src/router/index.ts
@@ -3,28 +3,45 @@ import {createRouter, createWebHistory} from 'vue-router'
 import AppLayout from '@/layout/AppLayout.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: AppLayout,
+      children: [
         {
-            path: '/',
-            // name: 'home',
-            component: AppLayout,
-            children: [
-                {
-                    path: '/',
-                    name: 'system-overview',
-                    // @ts-ignore
-                    component: () => import('@/views/SystemOverView.vue')
-                    // component: () => import('@/views/HomeView.vue')
-                },
-                // {
-                //     path: '/',
-                //     name: 'dashboard',
-                //     component: () => import('@/views/Dashboard.vue')
-                // },
-            ]
+          path: '',
+          name: 'system-overview',
+          component: () => import('@/views/SystemOverView.vue'),
+          props: true,
         },
-    ]
+        {
+          path: '/devices/:deviceId/temp/:name',
+          name: 'device-temp',
+          component: () => import('@/views/HomeView.vue'),
+          props: true,
+        },
+        {
+          path: '/devices/:deviceId/speed/:name',
+          name: 'device-speed',
+          component: () => import('@/views/HomeView.vue'),
+          props: true,
+        },
+        {
+          path: '/devices/:deviceId/lighting/:name',
+          name: 'device-lighting',
+          component: () => import('@/views/HomeView.vue'),
+          props: true,
+        },
+        {
+          path: '/devices/:deviceId/lcd/:name',
+          name: 'device-lcd',
+          component: () => import('@/views/HomeView.vue'),
+          props: true,
+        },
+      ]
+    },
+  ]
 })
 
 export default router
