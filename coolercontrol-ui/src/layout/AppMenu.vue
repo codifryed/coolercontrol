a4a4443ed53b18f3a9048bfe919f78a29f231cd2
Guy Boldon <gb@guyboldon.com>
2023-09-22 01:42:12 +0200

    ui: setup working device menu

--- coolercontrol-ui/src/layout/AppMenu.vue
+++ coolercontrol-ui/src/layout/AppMenu.vue
@@ -2,85 +2,158 @@
 import {ref} from 'vue'
 
 import AppMenuItem from './AppMenuItem.vue'
+import {useDeviceStore} from "@/stores/DeviceStore";
+import {ChannelStatus} from "@/models/Status";
 
-// todo: need to dynamically setup the menu from our app state (devices + channels as sub menus)
+const deviceStore = useDeviceStore()
+
 const model = ref([
-    {
-        label: '',
-        items: [
-            {label: 'System Overview', icon: 'pi pi-fw pi-chart-bar', to: '/'}
-        ]
-    },
-    {
-        label: '',
-        items: [
-            {
-                label: 'Submenu 1',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                    {
-                        label: 'Submenu 1.1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            {label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark'},
-                            {label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark'},
-                            {label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark'}
-                        ]
-                    },
-                    {
-                        label: 'Submenu 1.2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [{label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark'}]
-                    }
-                ]
-            },
-            {
-                label: 'Submenu 2',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                    {
-                        label: 'Submenu 2.1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            {label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark'},
-                            {label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark'}
-                        ]
-                    },
-                    {
-                        label: 'Submenu 2.2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [{label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark'}]
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        label: '',
-        items: [
-            {
-                label: 'Documentation',
-                icon: 'pi pi-fw pi-question',
-                to: '/documentation'
-            },
-            {
-                label: 'View Source',
-                icon: 'pi pi-fw pi-search',
-                url: 'https://github.com/primefaces/sakai-vue',
-                target: '_blank'
-            }
-        ]
-    }
+  {
+    label: '',
+    items: [
+      {label: 'System Overview', icon: 'pi pi-fw pi-chart-line', to: {name: 'system-overview'}}
+    ]
+  },
 ])
+
+const deviceItems = {
+  label: '',
+  items: [],
+}
+for (const device of deviceStore.allDevices()) {
+  const deviceItem = {
+    label: device.nameShort,
+    icon: 'pi pi-fw pi-server',
+    options: [
+      {
+        label: 'Hide',
+        // icon: 'pi pi-fw pi-check',
+      },
+      {
+        label: 'Ignore',
+        // icon: 'pi pi-fw pi-check',
+      }
+    ],
+    items: [],
+  }
+  for (const temp of device.status.temps) {
+    // @ts-ignore
+    deviceItem.items.push({
+      label: temp.frontend_name,
+      icon: 'pi pi-fw pi-minus',
+      iconStyle: `color: ${device.colors.getValue(temp.name)};`,
+      to: {name: 'device-temp', params: {deviceId: device.uid, name: temp.name}},
+      temp: temp.temp.toFixed(1),
+      options: [
+        {
+          label: 'Hide',
+          // icon: 'pi pi-fw pi-check',
+        },
+        {
+          label: 'Color',
+          // icon: 'pi pi-fw pi-check',
+        }
+      ],
+    })
+
+  }
+  if (device.info != null) {
+    for (const [channelName, channelInfo] of device.info.channels.entries()) {
+      if (channelInfo.speed_options) {
+        const foundMatchingSensors: ChannelStatus | undefined = device.status.channels
+            .find((channelStatus) => channelStatus.name === channelName)
+        const duty = foundMatchingSensors?.duty ?? 0
+        const rpm = foundMatchingSensors?.rpm ?? 0
+        // @ts-ignore
+        deviceItem.items.push({
+          label: channelName,
+          icon: 'pi pi-fw pi-minus',
+          // icon: icon,
+          iconStyle: `color: ${device.colors.getValue(channelName)};`,
+          to: {name: 'device-speed', params: {deviceId: device.uid, name: channelName}},
+          duty: duty.toFixed(1),
+          rpm: rpm,
+          options: [
+            {
+              label: 'Hide',
+              // icon: 'pi pi-fw pi-check',
+            },
+            {
+              label: 'Color',
+              // icon: 'pi pi-fw pi-check',
+            }
+          ],
+        });
+      } else if (channelInfo.lighting_modes.length > 0) {
+        // @ts-ignore
+        deviceItem.items.push({
+          label: channelName,
+          icon: 'pi pi-fw pi-minus',
+          // icon: icon,
+          iconStyle: `color: ${device.colors.getValue(channelName)};`,
+          to: {name: 'device-lighting', params: {deviceId: device.uid, name: channelName}},
+          options: [
+            {
+              label: 'Hide',
+              // icon: 'pi pi-fw pi-check',
+            },
+          ],
+        });
+      } else if (channelInfo.lcd_modes.length > 0) {
+        // @ts-ignore
+        deviceItem.items.push({
+          label: channelName,
+          icon: 'pi pi-fw pi-minus',
+          // icon: icon,
+          iconStyle: `color: ${device.colors.getValue(channelName)};`,
+          to: {name: 'device-lcd', params: {deviceId: device.uid, name: channelName}},
+          options: [
+            {
+              label: 'Hide',
+              // icon: 'pi pi-fw pi-check',
+            },
+          ],
+        });
+      }
+    }
+  }
+  // @ts-ignore
+  deviceItems.items.push(deviceItem)
+}
+model.value.push(deviceItems)
+model.value.push(
+    {
+      label: '',
+      items: [
+        {
+          label: 'Wiki',
+          icon: 'pi pi-fw pi-question-circle',
+          // @ts-ignore
+          url: 'https://gitlab.com/coolercontrol/coolercontrol/-/wikis/home',
+          target: '_blank'
+        },
+        {
+          label: 'Project Page',
+          icon: 'pi pi-fw pi-github',
+          // @ts-ignore
+          url: 'https://gitlab.com/coolercontrol/coolercontrol/',
+          target: '_blank'
+        }
+      ]
+    }
+)
 </script>
 
 <template>
-    <ul class="layout-menu">
-        <template v-for="(item, i) in model" :key="item">
-            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
-            <li v-if="item.separator" class="menu-separator"></li>
-        </template>
-    </ul>
+  <ul class="layout-menu">
+    <template v-for="(item, i) in model" :key="item">
+      <!--      <app-menu-item :item="item" :index="i"></app-menu-item>-->
+      <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
+      <li v-if="item.separator" class="menu-separator"></li>
+    </template>
+  </ul>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+//
+</style>
--- coolercontrol-ui/src/layout/AppMenuItem.vue
+++ coolercontrol-ui/src/layout/AppMenuItem.vue
@@ -1,93 +1,150 @@
 <script setup>
-import { ref, onBeforeMount, watch } from 'vue';
-import { useRoute } from 'vue-router';
-import { useLayout } from '@/layout/composables/layout';
+import {ref, onBeforeMount, watch} from 'vue';
+import {useRoute} from 'vue-router';
+import {useLayout} from '@/layout/composables/layout';
+import Button from 'primevue/button';
+import Menu from 'primevue/menu';
 
 const route = useRoute();
 
-const { layoutConfig, layoutState, setActiveMenuItem, onMenuToggle } = useLayout();
+const {layoutConfig, layoutState, setActiveMenuItem, onMenuToggle} = useLayout();
 
 const props = defineProps({
-    item: {
-        type: Object,
-        default: () => ({})
-    },
-    index: {
-        type: Number,
-        default: 0
-    },
-    root: {
-        type: Boolean,
-        default: true
-    },
-    parentItemKey: {
-        type: String,
-        default: null
-    }
+  item: {
+    type: Object,
+    default: () => ({})
+  },
+  index: {
+    type: Number,
+    default: 0
+  },
+  root: {
+    type: Boolean,
+    default: true
+  },
+  parentItemKey: {
+    type: String,
+    default: null
+  }
 });
 
-const isActiveMenu = ref(false);
+const isActiveMenu = ref(true);
 const itemKey = ref(null);
 
 onBeforeMount(() => {
-    itemKey.value = props.parentItemKey ? props.parentItemKey + '-' + props.index : String(props.index);
+  itemKey.value = props.parentItemKey ? props.parentItemKey + '-' + props.index : String(props.index);
 
-    const activeItem = layoutState.activeMenuItem;
+  // todo: enable simple toggle in the menu, not only one open and all open on start
+  // const activeItem = layoutState.activeMenuItem;
 
-    isActiveMenu.value = activeItem === itemKey.value || activeItem ? activeItem.startsWith(itemKey.value + '-') : false;
+  // isActiveMenu.value = activeItem === itemKey.value || activeItem ? activeItem.startsWith(itemKey.value + '-') : false;
 });
 
-watch(
-    () => layoutConfig.activeMenuItem.value,
-    (newVal) => {
-        isActiveMenu.value = newVal === itemKey.value || newVal.startsWith(itemKey.value + '-');
-    }
-);
+// watch(
+//     () => layoutConfig.activeMenuItem.value,
+//     (newVal) => {
+//       isActiveMenu.value = newVal === itemKey.value || newVal.startsWith(itemKey.value + '-');
+//     }
+// );
 const itemClick = (event, item) => {
-    if (item.disabled) {
-        event.preventDefault();
-        return;
-    }
+  if (item.disabled) {
+    event.preventDefault();
+    return;
+  }
 
-    const { overlayMenuActive, staticMenuMobileActive } = layoutState;
+  const {overlayMenuActive, staticMenuMobileActive} = layoutState;
 
-    if ((item.to || item.url) && (staticMenuMobileActive.value || overlayMenuActive.value)) {
-        onMenuToggle();
-    }
+  if ((item.to || item.url) && (staticMenuMobileActive.value || overlayMenuActive.value)) {
+    onMenuToggle();
+  }
 
-    if (item.command) {
-        item.command({ originalEvent: event, item: item });
-    }
+  if (item.command) {
+    item.command({originalEvent: event, item: item});
+  }
 
-    const foundItemKey = item.items ? (isActiveMenu.value ? props.parentItemKey : itemKey) : itemKey.value;
+  // const foundItemKey = item.items ? (isActiveMenu.value ? props.parentItemKey : itemKey) : itemKey.value;
 
-    setActiveMenuItem(foundItemKey);
+  // setActiveMenuItem(foundItemKey);
+  isActiveMenu.value = !isActiveMenu.value;  // very simply toggle
+  // todo: save state
 };
 
-const checkActiveRoute = (item) => {
-    return route.path === item.to;
+const optionsMenu = ref();
+const optionsToggle = (event) => {
+  optionsMenu.value.toggle(event);
 };
 </script>
 
 <template>
-    <li :class="{ 'layout-root-menuitem': root, 'active-menuitem': isActiveMenu }">
-        <div v-if="root && item.visible !== false" class="layout-menuitem-root-text">{{ item.label }}</div>
-        <a v-if="(!item.to || item.items) && item.visible !== false" :href="item.url" @click="itemClick($event, item, index)" :class="item.class" :target="item.target" tabindex="0">
-            <i :class="item.icon" class="layout-menuitem-icon"></i>
-            <span class="layout-menuitem-text">{{ item.label }}</span>
-            <i class="pi pi-fw pi-angle-down layout-submenu-toggler" v-if="item.items"></i>
-        </a>
-        <router-link v-if="item.to && !item.items && item.visible !== false" @click="itemClick($event, item, index)" :class="[item.class, { 'active-route': checkActiveRoute(item) }]" tabindex="0" :to="item.to">
-            <i :class="item.icon" class="layout-menuitem-icon"></i>
-            <span class="layout-menuitem-text">{{ item.label }}</span>
-            <i class="pi pi-fw pi-angle-down layout-submenu-toggler" v-if="item.items"></i>
-        </router-link>
-        <Transition v-if="item.items && item.visible !== false" name="layout-submenu">
-            <ul v-show="root ? true : isActiveMenu" class="layout-submenu">
-                <app-menu-item v-for="(child, i) in item.items" :key="child" :index="i" :item="child" :parentItemKey="itemKey" :root="false"></app-menu-item>
-            </ul>
-        </Transition>
-    </li>
+  <li :class="{ 'layout-root-menuitem': root, 'active-menuitem': isActiveMenu }">
+    <div v-if="root && item.visible !== false" class="layout-menuitem-root-text">{{ item.label }}</div>
+    <a v-if="(!item.to || item.items) && item.visible !== false" :href="item.url"
+       @click="itemClick($event, item, index)"
+       :class="item.class" :target="item.target" tabindex="0">
+      <i :class="item.icon" class="layout-menuitem-icon" :style="item.iconStyle"></i>
+      <span class="layout-menuitem-text">{{ item.label }}</span>
+      <span class="layout-menuitem-text ml-auto"></span>
+      <i class="pi pi-fw pi-angle-down layout-submenu-toggler" v-if="item.items"></i>
+      <Button v-if="item.options" aria-label="options" icon="pi pi-ellipsis-v" rounded text plain size="small"
+              class="ml-1 p-3 channel-options"
+              style="height: 0.1rem; width: 0.1rem; box-shadow: none; visibility: hidden"
+              type="button" aria-haspopup="true" @click.stop.prevent="optionsToggle"/>
+      <Menu ref="optionsMenu" :model="item.options" :popup="true">
+        <template #item="{ label, item, props }">
+          <a class="flex" v-bind="props.action">
+            <span v-bind="props.icon"/><span v-bind="props.label">{{ label }}</span>
+          </a>
+        </template>
+      </Menu>
+    </a>
+
+    <router-link v-if="item.to && !item.items && item.visible !== false" @click="itemClick($event, item, index)"
+                 :class="[item.class, 'device-channel']" exact-active-class="active-route" exact
+                 tabindex="0" :to="item.to">
+      <i :class="item.icon" class="layout-menuitem-icon" :style="item.iconStyle"></i>
+      <span class="layout-menuitem-text">{{ item.label }}</span>
+      <i class="pi pi-fw pi-angle-down layout-submenu-toggler" v-if="item.items"></i>
+      <span v-if="item.temp" class="layout-menuitem-text ml-auto">{{ item.temp }}<span
+          class="ml-1">°&nbsp;&nbsp;&nbsp;</span></span>
+      <span v-else-if="(item.duty && !item.rpm && item.rpm !== 0)" class="layout-menuitem-text ml-auto text-right">
+        {{ item.duty }}<span style="font-size: 0.7rem"> %&nbsp;&nbsp;&nbsp;</span>
+      </span>
+      <span v-else-if="(!item.duty && item.rpm != null)" class="layout-menuitem-text ml-auto text-right">
+        {{ item.rpm }}<span style="font-size: 0.7rem">rpm</span>
+      </span>
+      <span v-else-if="(item.duty && item.rpm != null)" class="layout-menuitem-text ml-auto text-right">
+        {{ item.duty }}<span class="ml-1" style="font-size: 0.7rem">%&nbsp;&nbsp;&nbsp;</span><br/>{{ item.rpm }}<span
+          class="ml-1"
+          style="font-size: 0.7rem">rpm</span>
+      </span>
+      <span v-else class="layout-menuitem-text ml-auto"></span>
+      <Button v-if="item.options" aria-label="options" icon="pi pi-ellipsis-v" rounded text plain size="small"
+              class="ml-1 p-3 channel-options"
+              style="height: 0.1rem; width: 0.1rem; box-shadow: none; visibility: hidden"
+              type="button" aria-haspopup="true" @click.stop.prevent="optionsToggle"/>
+      <Button v-else icon="pi pi-ellipsis-v" rounded text plain size="small"
+              class="ml-1 p-3"
+              style="height: 0.1rem; width: 0.1rem; box-shadow: none; visibility: hidden"
+              type="button"/>
+      <Menu ref="optionsMenu" :model="item.options" :popup="true">
+        <template #item="{ label, item, props }">
+          <a class="flex" v-bind="props.action">
+            <span v-bind="props.icon"/><span v-bind="props.label">{{ label }}</span>
+          </a>
+        </template>
+      </Menu>
+    </router-link>
+    <Transition v-if="item.items && item.visible !== false" name="layout-submenu">
+      <ul v-show="root ? true : isActiveMenu" class="layout-submenu">
+        <!--      <ul v-show=true class="layout-submenu">-->
+        <app-menu-item v-for="(child, i) in item.items" :key="child" :index="i" :item="child" :parentItemKey="itemKey"
+                       :root="false">
+        </app-menu-item>
+      </ul>
+    </Transition>
+  </li>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+//
+</style>
