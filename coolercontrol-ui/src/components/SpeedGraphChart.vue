33e04fa243dde230739662b83c2b63cdf784a40d
Guy Boldon <gb@guyboldon.com>
2023-10-13 22:30:49 +0200

    ui: implement graph speed chart

--- coolercontrol-ui/src/components/SpeedGraphChart.vue
+++ coolercontrol-ui/src/components/SpeedGraphChart.vue
@@ -17,13 +17,278 @@
   -->
 
 <script setup lang="ts">
+import * as echarts from 'echarts/core'
+import {GraphicComponent, GridComponent, MarkAreaComponent, TooltipComponent,} from 'echarts/components'
+import {LineChart} from 'echarts/charts'
+import {UniversalTransition} from 'echarts/features'
+import {CanvasRenderer} from 'echarts/renderers'
+import VChart from 'vue-echarts'
+import {type EChartsOption} from "echarts"
+import {Profile} from "@/models/Profile"
+import {type UID} from "@/models/Device"
+import {useDeviceStore} from "@/stores/DeviceStore"
+import {storeToRefs} from "pinia"
+import {useSettingsStore} from "@/stores/SettingsStore"
+import {useThemeColorsStore} from "@/stores/ThemeColorsStore"
+import {onMounted, ref, watch} from "vue"
 
+echarts.use([
+  GridComponent, LineChart, CanvasRenderer, UniversalTransition, TooltipComponent, GraphicComponent, MarkAreaComponent
+])
+
+interface Props {
+  profile: Profile
+  currentDeviceUID: UID
+  currentSensorName: string
+}
+
+const props = defineProps<Props>()
+
+const deviceStore = useDeviceStore()
+const {currentDeviceStatus} = storeToRefs(deviceStore)
+const settingsStore = useSettingsStore()
+const colors = useThemeColorsStore()
+
+const axisXTempMin: number = 0
+const axisXTempMax: number = 100
+const dutyMin: number = 0
+const dutyMax: number = 100
+
+interface LineData {
+  value: number[]
+}
+
+const deviceDutyLineData: [LineData, LineData] = [{value: []}, {value: []}]
+const tempLineData: [LineData, LineData] = [{value: []}, {value: []}]
+const graphLineData: Array<LineData> = []
+
+const getDeviceDutyLineColor = (): string => {
+  return settingsStore.allDeviceSettings.get(props.currentDeviceUID)?.sensorsAndChannels
+      .getValue(props.currentSensorName)
+      .color ?? colors.themeColors().yellow
+}
+const getTempLineColor = (): string => {
+  if (props.profile.temp_source == null) {
+    return colors.themeColors().yellow
+  }
+  return settingsStore.allDeviceSettings
+      .get(props.profile.temp_source.device_uid)?.sensorsAndChannels
+      .getValue(props.profile.temp_source.temp_name)
+      .color ?? colors.themeColors().yellow
+}
+
+const initOptions = {
+  useDirtyRect: false, // true causes some issues with animations and opaque lines
+  renderer: 'canvas',
+}
+
+const option: EChartsOption = {
+  grid: {
+    show: false,
+    top: 10,
+    left: 10,
+    right: 15,
+    bottom: 0,
+    containLabel: true,
+  },
+  xAxis: {
+    min: axisXTempMin,
+    max: axisXTempMax,
+    type: 'value',
+    splitNumber: 10,
+    axisLabel: {
+      formatter: '{value}°'
+    },
+    axisLine: {
+      lineStyle: {
+        color: colors.themeColors().text_active,
+        width: 1,
+      }
+    },
+    splitLine: {
+      lineStyle: {
+        color: colors.themeColors().text_description,
+        type: 'dotted'
+      }
+    },
+  },
+  yAxis: {
+    min: dutyMin,
+    max: dutyMax,
+    type: 'value',
+    splitNumber: 10,
+    axisLabel: {
+      formatter: '{value}%'
+    },
+    axisLine: {
+      lineStyle: {
+        color: colors.themeColors().text_active,
+        width: 1,
+      }
+    },
+    splitLine: {
+      lineStyle: {
+        color: colors.themeColors().text_description,
+        type: 'dotted'
+      }
+    },
+  },
+  // @ts-ignore
+  series: [
+    {
+      id: 'dutyLine',
+      type: 'line',
+      smooth: false,
+      symbol: 'none',
+      lineStyle: {
+        color: getDeviceDutyLineColor(),
+        width: 2,
+        type: 'solid',
+      },
+      emphasis: {
+        disabled: true,
+      },
+      data: deviceDutyLineData,
+      z: 100,
+      silent: true,
+    },
+    {
+      id: 'tempLine',
+      type: 'line',
+      smooth: false,
+      symbol: 'none',
+      lineStyle: {
+        color: getTempLineColor(),
+        width: 1,
+        type: 'dashed',
+      },
+      emphasis: {
+        disabled: true,
+      },
+      data: tempLineData,
+      z: 10,
+      silent: true,
+    },
+    {
+      id: 'GraphLine',
+      type: 'line',
+      smooth: 0.05,
+      symbol: 'none',
+      lineStyle: {
+        color: {
+          type: 'linear',
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [{
+            offset: 0, color: `${colors.themeColors().green}00`
+          }, {
+            offset: 0.1, color: `${colors.themeColors().green}80`
+          }, {
+            offset: 0.5, color: `${colors.themeColors().green}90`
+          }, {
+            offset: 0.1, color: `${colors.themeColors().green}80`
+          }, {
+            offset: 1, color: `${colors.themeColors().green}00`
+          }],
+        },
+        width: 12,
+        cap: 'round',
+      },
+      emphasis: {
+        disabled: true,
+      },
+      data: graphLineData,
+      z: 1,
+      silent: true,
+    },
+  ],
+  animation: true,
+  animationDurationUpdate: 800,
+}
+
+const getDuty = (): number => {
+  return Number(currentDeviceStatus.value.get(props.currentDeviceUID)?.get(props.currentSensorName)?.duty) ?? 0
+}
+
+const getTemp = (): number => {
+  if (props.profile.temp_source == null) {
+    return 0
+  }
+  const tempValue = deviceStore.currentDeviceStatus
+      .get(props.profile.temp_source.device_uid)
+      ?.get(props.profile.temp_source.temp_name)
+      ?.temp
+  if (tempValue == null) {
+    return 0
+  }
+  return Number(tempValue)
+}
+
+const setGraphData = () => {
+  const duty = getDuty()
+  deviceDutyLineData[0].value = [axisXTempMin, duty]
+  deviceDutyLineData[1].value = [axisXTempMax, duty]
+  const temp = getTemp()
+  tempLineData[0].value = [temp, dutyMin]
+  tempLineData[1].value = [temp, dutyMax]
+  graphLineData.length = 0
+  if (props.profile.speed_profile.length > 1) {
+    for (const point of props.profile.speed_profile) {
+      graphLineData.push({value: point})
+    }
+  }
+}
+setGraphData()
+
+const controlGraph = ref<InstanceType<typeof VChart> | null>(null)
+
+watch(currentDeviceStatus, () => {
+  const duty = getDuty()
+  if (duty === 0) {
+    return
+  }
+  deviceDutyLineData[0].value = [axisXTempMin, duty]
+  deviceDutyLineData[1].value = [axisXTempMax, duty]
+  const temp = getTemp()
+  tempLineData[0].value = [temp, dutyMin]
+  tempLineData[1].value = [temp, dutyMax]
+  controlGraph.value?.setOption({
+    series: [
+      {id: 'dutyLine', data: deviceDutyLineData}, {id: 'tempLine', data: tempLineData}
+    ]
+  })
+})
+
+watch(settingsStore.allDeviceSettings, () => {
+  const dutyLineColor = getDeviceDutyLineColor()
+  const tempLineColor = getTempLineColor()
+  // @ts-ignore
+  option.series[0].lineStyle.color = dutyLineColor
+  // @ts-ignore
+  option.series[1].lineStyle.color = tempLineColor
+  controlGraph.value?.setOption({
+    series: [
+      {id: 'dutyLine', lineStyle: {color: dutyLineColor}},
+      {id: 'tempLine', lineStyle: {color: tempLineColor}},
+    ]
+  })
+})
+
+onMounted(() => {
+  controlGraph.value?.setOption(option)
+})
 </script>
 
 <template>
-
+  <v-chart class="control-graph" ref="controlGraph" :init-options="initOptions"
+           :autoresize="true" :manual-update="true"/>
 </template>
 
 <style scoped lang="scss">
-
+.control-graph {
+  height: 80vh;
+  width: 99.9%; // This handles an issue with the graph when the layout thinks it's too big for the container
+}
 </style>
