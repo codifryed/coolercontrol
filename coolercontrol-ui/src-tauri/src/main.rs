d86eca000d035cbc08be5bf89d3e9571a488dd14
Guy Boldon <gb@guyboldon.com>
2023-10-29 11:46:45 +0100

    ui: enable tauri localhost in production and clipboard api

--- coolercontrol-ui/src-tauri/src/main.rs
+++ coolercontrol-ui/src-tauri/src/main.rs
@@ -19,11 +19,32 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{AppHandle, Manager, SystemTray, SystemTrayEvent};
+use tauri::{AppHandle, Context, Manager, SystemTray, SystemTrayEvent, WindowUrl};
 use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::utils::assets::EmbeddedAssets;
+use tauri::utils::config::AppUrl;
 use tauri_plugin_autostart::MacosLauncher;
 
+const DAEMON_ADDRESS: &str = "http://localhost";
+const DAEMON_PORT: u16 = 11987;
+
 fn main() {
+    tauri::Builder::default()
+        .system_tray(create_sys_tray())
+        .on_system_tray_event(|app, event| handle_sys_tray_event(app, event))
+        // .on_window_event(|event| handle_window_event(event))
+        .plugin(tauri_plugin_localhost::Builder::new(DAEMON_PORT).build())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+            app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
+        }))
+        .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, Some(vec![])))
+        .run(create_context())
+        .expect("error while running tauri application");
+}
+
+fn create_sys_tray() -> SystemTray {
     let tray_menu_item_cc = CustomMenuItem::new("cc".to_string(), "CoolerControl").disabled();
     let tray_menu_item_hide = CustomMenuItem::new("hide".to_string(), "Hide");
     let tray_menu_item_quit = CustomMenuItem::new("quit".to_string(), "Quit");
@@ -32,20 +53,15 @@ fn main() {
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(tray_menu_item_hide)
         .add_item(tray_menu_item_quit);
-    let system_tray = SystemTray::new()
-        .with_menu(tray_menu);
-    tauri::Builder::default()
-        .system_tray(system_tray)
-        .on_system_tray_event(|app, event| handle_sys_tray_event(app, event))
-        // .on_window_event(|event| handle_window_event(event))
-        .plugin(tauri_plugin_window_state::Builder::default().build())
-        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
-            println!("{}, {argv:?}, {cwd}", app.package_info().name);
-            app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
-        }))
-        .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, Some(vec![])))
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    SystemTray::new().with_menu(tray_menu)
+}
+
+fn create_context() -> Context<EmbeddedAssets> {
+    let mut context = tauri::generate_context!();
+    let url = format!("{}:{}", DAEMON_ADDRESS, DAEMON_PORT).parse().unwrap();
+    // rewrite the config so the IPC is enabled on this URL
+    context.config_mut().build.dist_dir = AppUrl::Url(WindowUrl::External(url));
+    context
 }
 
 fn handle_sys_tray_event(app: &AppHandle, event: SystemTrayEvent) {
