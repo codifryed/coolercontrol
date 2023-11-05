5a79dad1fcd80e97a61c99f81d3b6bcee8998e17
Guy Boldon <gb@guyboldon.com>
2023-11-05 23:17:35 +0100

    ui: implement base Settings/Config Page

--- coolercontrol-ui/src/layout/AppConfig.vue
+++ coolercontrol-ui/src/layout/AppConfig.vue
@@ -16,59 +16,86 @@
   - along with this program.  If not, see <https://www.gnu.org/licenses/>.
   -->
 
-<script setup>
-import RadioButton from 'primevue/radiobutton';
-import Button from 'primevue/button';
-import InputSwitch from 'primevue/inputswitch';
-import Sidebar from 'primevue/sidebar';
+<script setup lang="ts">
+import Button from 'primevue/button'
+import Sidebar from 'primevue/sidebar'
+import SelectButton from 'primevue/selectbutton'
+import Divider from 'primevue/divider'
+import InputNumber from 'primevue/inputnumber'
 
-import {ref} from 'vue';
-import {useLayout} from '@/layout/composables/layout';
+import {type Ref, ref} from 'vue'
+import {useLayout} from '@/layout/composables/layout'
+import {useDeviceStore} from "@/stores/DeviceStore";
+import {useSettingsStore} from "@/stores/SettingsStore";
 
 defineProps({
   simple: {
     type: Boolean,
     default: true
   }
-});
-const scales = ref([12, 13, 14, 15, 16]);
+})
 
-const {changeThemeSettings, setScale, layoutConfig, onConfigButtonClick, isConfigSidebarActive} = useLayout();
+const scales = ref([12, 13, 14, 15, 16, 17, 18, 19, 20])
 
+const {changeThemeSettings, setScale, layoutConfig, onConfigButtonClick, isConfigSidebarActive} = useLayout()
+
+const deviceStore = useDeviceStore()
+const settingsStore = useSettingsStore()
 const appVersion = import.meta.env.PACKAGE_VERSION
 
-// todo: change this to be able to switch our dark & light theme. (at first disable light - needs work)
-const onChangeTheme = (theme, mode) => {
-  const elementId = 'theme-css';
-  const linkElement = document.getElementById(elementId);
-  const cloneLinkElement = linkElement.cloneNode(true);
-  const newThemeUrl = linkElement.getAttribute('href').replace(layoutConfig.theme.value, theme);
-  cloneLinkElement.setAttribute('id', elementId + '-clone');
-  cloneLinkElement.setAttribute('href', newThemeUrl);
-  cloneLinkElement.addEventListener('load', () => {
-    linkElement.remove();
-    cloneLinkElement.setAttribute('id', elementId);
-    changeThemeSettings(theme, mode === 'dark');
-  });
-  linkElement.parentNode.insertBefore(cloneLinkElement, linkElement.nextSibling);
-};
+// const applyOnBootEnabled: Ref<boolean> = ref(settingsStore.)
+
+// todo: refactor this to be able to switch our dark & light theme:
+// const onChangeTheme = (theme, mode) => {
+//   const elementId = 'theme-css';
+//   const linkElement = document.getElementById(elementId);
+//   const cloneLinkElement = linkElement.cloneNode(true);
+//   const newThemeUrl = linkElement.getAttribute('href').replace(layoutConfig.theme.value, theme);
+//   cloneLinkElement.setAttribute('id', elementId + '-clone');
+//   cloneLinkElement.setAttribute('href', newThemeUrl);
+//   cloneLinkElement.addEventListener('load', () => {
+//     linkElement.remove();
+//     cloneLinkElement.setAttribute('id', elementId);
+//     changeThemeSettings(theme, mode === 'dark');
+//   });
+//   linkElement.parentNode.insertBefore(cloneLinkElement, linkElement.nextSibling);
+// };
+
 const decrementScale = () => {
-  setScale(layoutConfig.scale.value - 1);
-  applyScale();
-};
+  setScale(layoutConfig.scale.value - 1)
+  applyScale()
+}
 const incrementScale = () => {
-  setScale(layoutConfig.scale.value + 1);
-  applyScale();
-};
+  setScale(layoutConfig.scale.value + 1)
+  applyScale()
+}
 const applyScale = () => {
-  document.documentElement.style.fontSize = layoutConfig.scale.value + 'px';
-};
+  document.documentElement.style.fontSize = layoutConfig.scale.value + 'px'
+}
+
+const enabledOptions = [
+  {value: true, label: 'Enabled'},
+  {value: false, label: 'Disabled'},
+]
+const menuLayoutOptions = ['static', 'overlay']
+const noInitOptions = [
+  {value: false, label: 'Enabled'},
+  {value: true, label: 'Disabled'},
+]
+
 </script>
 
 <template>
   <Sidebar v-model:visible="isConfigSidebarActive" position="right"
            :transitionOptions="'.3s cubic-bezier(0, 0, 0.2, 1)'" class="layout-config-sidebar w-30rem">
-    <h5>Scale</h5>
+    <h3>CoolerControl</h3>
+    v{{ appVersion }}
+    <p>
+      This program comes with absolutely no warranty.
+    </p>
+    <Divider/>
+
+    <h6>UI Scale</h6>
     <div class="flex align-items-center">
       <Button icon="pi pi-minus" type="button" @click="decrementScale()"
               class="p-button-text p-button-rounded w-2rem h-2rem mr-2"
@@ -82,86 +109,55 @@ const applyScale = () => {
               :disabled="layoutConfig.scale.value === scales[scales.length - 1]"></Button>
     </div>
 
-    <template v-if="!simple">
-      <h5>Menu Type</h5>
-      <div class="flex">
-        <div class="field-radiobutton flex-1">
-          <RadioButton name="menuMode" value="static" v-model="layoutConfig.menuMode.value"
-                       inputId="mode1"></RadioButton>
-          <label for="mode1">Static</label>
-        </div>
+    <h6>Menu Type</h6>
+    <div class="flex">
+      <SelectButton v-model="layoutConfig.menuMode.value" :options="menuLayoutOptions"
+                    :option-label="(value: string) => deviceStore.toTitleCase(value)"
+                    :unselectable="true"/>
+    </div>
 
-        <div class="field-radiobutton flex-1">
-          <RadioButton name="menuMode" value="overlay" v-model="layoutConfig.menuMode.value"
-                       inputId="mode2"></RadioButton>
-          <label for="mode2">Overlay</label>
-        </div>
-      </div>
-    </template>
+    <h6>Apply Settings on System Boot</h6>
+    <div class="flex">
+      <SelectButton v-model="settingsStore.ccSettings.apply_on_boot" :options="enabledOptions" option-label="label"
+                    option-value="value" :unselectable="true"
+                    v-tooltip.left="'Whether to apply your settings automatically when the daemon starts'"/>
+      <!--      :allowEmpty="false"-->
+    </div>
 
-    <template v-if="!simple">
-      <h5>Input Style</h5>
-      <div class="flex">
-        <div class="field-radiobutton flex-1">
-          <RadioButton name="inputStyle" value="outlined" v-model="layoutConfig.inputStyle.value"
-                       inputId="outlined_input"></RadioButton>
-          <label for="outlined_input">Outlined</label>
-        </div>
-        <div class="field-radiobutton flex-1">
-          <RadioButton name="inputStyle" value="filled" v-model="layoutConfig.inputStyle.value"
-                       inputId="filled_input"></RadioButton>
-          <label for="filled_input">Filled</label>
-        </div>
-      </div>
+    <h6>Liquidctl Device Initialization</h6>
+    <div class="flex">
+      <SelectButton v-model="settingsStore.ccSettings.no_init" :options="noInitOptions" option-label="label"
+                    option-value="value" :unselectable="true"
+                    v-tooltip.left="'Disabling this can help avoid conflicts with other programs that also control ' +
+                     'your liquidctl devices. Most devices require this step for proper communication and should ' +
+                      'only be disabled with care.'"/>
+      <!--      :allowEmpty="false"-->
+    </div>
 
-      <h5>Ripple Effect</h5>
-      <InputSwitch v-model="layoutConfig.ripple.value"></InputSwitch>
-    </template>
+    <h6>Boot-Up Delay</h6>
+    <div class="flex">
+      <InputNumber v-model="settingsStore.ccSettings.startup_delay" showButtons :min="1" :max="10" suffix=" seconds"
+                   v-tooltip.left="'The number of seconds the daemon waits before attempting to communicate ' +
+                    'with devices. This can be helpful when dealing with devices that aren\'t consistently detected' +
+                     ' or need extra time to fully initialize.'"/>
+    </div>
 
-    <!--        <h5>PrimeOne Design - 2022</h5>-->
-    <!--        <div class="grid">-->
-    <!--            <div class="col-3">-->
-    <!--                <button class="p-link w-2rem h-2rem" @click="onChangeTheme('lara-light-indigo', 'light')">-->
-    <!--                    <img src="/layout/images/themes/lara-light-indigo.png" class="w-2rem h-2rem" alt="Lara Light Indigo" />-->
-    <!--                </button>-->
-    <!--            </div>-->
-    <!--            <div class="col-3">-->
-    <!--                <button class="p-link w-2rem h-2rem" @click="onChangeTheme('lara-light-blue', 'light')">-->
-    <!--                    <img src="/layout/images/themes/lara-light-blue.png" class="w-2rem h-2rem" alt="Lara Light Blue" />-->
-    <!--                </button>-->
-    <!--            </div>-->
-    <!--            <div class="col-3">-->
-    <!--                <button class="p-link w-2rem h-2rem" @click="onChangeTheme('lara-light-purple', 'light')">-->
-    <!--                    <img src="/layout/images/themes/lara-light-purple.png" class="w-2rem h-2rem" alt="Lara Light Purple" />-->
-    <!--                </button>-->
-    <!--            </div>-->
-    <!--            <div class="col-3">-->
-    <!--                <button class="p-link w-2rem h-2rem" @click="onChangeTheme('lara-light-teal', 'light')">-->
-    <!--                    <img src="/layout/images/themes/lara-light-teal.png" class="w-2rem h-2rem" alt="Lara Light Teal" />-->
-    <!--                </button>-->
-    <!--            </div>-->
-    <!--            <div class="col-3">-->
-    <!--                <button class="p-link w-2rem h-2rem" @click="onChangeTheme('lara-dark-indigo', 'dark')">-->
-    <!--                    <img src="/layout/images/themes/lara-dark-indigo.png" class="w-2rem h-2rem" alt="Lara Dark Indigo" />-->
-    <!--                </button>-->
-    <!--            </div>-->
-    <!--            <div class="col-3">-->
-    <!--                <button class="p-link w-2rem h-2rem" @click="onChangeTheme('lara-dark-blue', 'dark')">-->
-    <!--                    <img src="/layout/images/themes/lara-dark-blue.png" class="w-2rem h-2rem" alt="Lara Dark Blue" />-->
-    <!--                </button>-->
-    <!--            </div>-->
-    <!--            <div class="col-3">-->
-    <!--                <button class="p-link w-2rem h-2rem" @click="onChangeTheme('lara-dark-purple', 'dark')">-->
-    <!--                    <img src="/layout/images/themes/lara-dark-purple.png" class="w-2rem h-2rem" alt="Lara Dark Purple" />-->
-    <!--                </button>-->
-    <!--            </div>-->
-    <!--            <div class="col-3">-->
-    <!--                <button class="p-link w-2rem h-2rem" @click="onChangeTheme('lara-dark-teal', 'dark')">-->
-    <!--                    <img src="/layout/images/themes/lara-dark-teal.png" class="w-2rem h-2rem" alt="Lara Dark Teal" />-->
-    <!--                </button>-->
-    <!--            </div>-->
-    <!--          <span class="font-lite text-sm ml-auto mr-5">v{{appVersion}}</span>-->
-    <!--        </div>-->
+    <h6>Thinkpad Full Speed</h6>
+    <div class="flex">
+      <SelectButton v-model="settingsStore.ccSettings.thinkpad_full_speed" :options="enabledOptions"
+                    option-label="label" option-value="value" :unselectable="true"
+                    v-tooltip.left="'For Thinkpad Laptops this enables Full-Speed mode. This allows the fans to ' +
+                     'spin up to their absolute maximum when set to 100%, but will run the fans out of ' +
+                      'specification and cause increased wear. Use with caution.'"/>
+    </div>
+
+    <!--    todo: enable thinkpad fan control helper-->
+
+    <!--    todo: Blacklisted Device List-->
+
+    <!--<button class="p-link w-2rem h-2rem" @click="onChangeTheme('lara-dark-teal', 'dark')">-->
+    <!--    <img src="/layout/images/themes/lara-dark-teal.png" class="w-2rem h-2rem" alt="Lara Dark Teal" />-->
+    <!--</button>-->
   </Sidebar>
 </template>
 
