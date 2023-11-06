a010984d3a7c5c45605dcac6b6522fc9d3e5902f
Guy Boldon <gb@guyboldon.com>
2023-11-06 20:55:25 +0100

    ui: tweak startup
    don't need that much time anymore

--- coolercontrol-ui/src/App.vue
+++ coolercontrol-ui/src/App.vue
@@ -44,7 +44,7 @@ onMounted(async () => {
     return
   }
   await settingsStore.initializeSettings(deviceStore.allDevices())
-  await sleep(200) // give the engine a moment to catch up for a smoother start
+  await sleep(100) // give the engine a moment to catch up for a smoother start
   loading.value = false
 
   const delay = () => new Promise(resolve => setTimeout(resolve, 200))
