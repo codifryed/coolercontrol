6cd449ce19bf4ab6a347499dcdbe87f756c23265
Guy Boldon <gb@guyboldon.com>
2024-01-06 16:38:33 +0100

    ci: update linter configs

--- .trunk/configs/svgo.config.js
+++ .trunk/configs/svgo.config.js
@@ -1,17 +1,17 @@
-/* 
+/*
  *  CoolerControl - monitor and control your cooling and other devices
  *  Copyright (c) 2023  Guy Boldon
- *  
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -19,7 +19,7 @@
 module.exports = {
     plugins: [
         {
-            name: "preset-default",
+            name: 'preset-default',
             params: {
                 overrides: {
                     removeViewBox: false, // https://github.com/svg/svgo/issues/1128
@@ -29,4 +29,4 @@ module.exports = {
             },
         },
     ],
-};
+}
