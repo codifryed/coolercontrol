4a52fcc720e1330c61664470be63590d9dd08b72
Guy Boldon <gb@guyboldon.com>
2024-01-16 23:19:01 +0100

    ui: various speed view improvements for devices that don't have duty output

--- coolercontrol-ui/src/views/SpeedView.vue
+++ coolercontrol-ui/src/views/SpeedView.vue
@@ -252,7 +252,7 @@ const saveSetting = async () => {
                             :key="'temp' + props.deviceId + props.name + selectedProfile.uid"
                             temp
                         />
-                        <MiniGauge
+                        <MiniGauge v-if="getCurrentDuty() != null"
                             :device-u-i-d="props.deviceId"
                             :sensor-name="props.name"
                             :key="'duty' + props.deviceId + props.name + selectedProfile.uid"
