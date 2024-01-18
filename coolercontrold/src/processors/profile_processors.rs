0c59e0e7e070178c98927b587acc7d0ebe19e724
Eren <eren.simsek@tuta.io>
2024-01-18 20:53:37 +0000

    Read different temperatures

--- coolercontrold/src/processors/profile_processors.rs
+++ coolercontrold/src/processors/profile_processors.rs
@@ -16,11 +16,19 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-use async_trait::async_trait;
+use std::collections::HashMap;
+use std::sync::Arc;
 
-use crate::device::UID;
+use async_trait::async_trait;
+use log::error;
+use tokio::sync::RwLock;
+
+use crate::device::{Device, UID};
 use crate::processors::{utils, Processor, SpeedProfileData};
 use crate::setting::{FunctionType, ProfileType};
+use crate::AllDevices;
+
+use super::NormalizedProfile;
 
 /// The standard Graph Profile processor that calculates duty from interpolating the speed profile.
 pub struct GraphProfileProcessor {}
@@ -31,11 +39,13 @@ impl GraphProfileProcessor {
     }
 }
 
-pub struct MixProfileProcessor {}
+pub struct MixProfileProcessor {
+    all_devices: AllDevices,
+}
 
 impl MixProfileProcessor {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(all_devices: AllDevices) -> Self {
+        Self { all_devices }
     }
 }
 
@@ -69,14 +79,56 @@ impl Processor for MixProfileProcessor {
     async fn clear_state(&self, _device_uid: &UID, _channel_name: &str) {}
 
     async fn process<'a>(&'a self, data: &'a mut SpeedProfileData) -> &'a mut SpeedProfileData {
-        let member_duties: Vec<u8> = data
+        let temp_source_device_options = self
+            .all_devices
+            .get(data.profile.temp_source.device_uid.as_str());
+        if temp_source_device_options.is_none() {}
+
+        let temp_source_devices = data
             .profile
             .member_profiles
             .iter()
-            .map(|member_profile| {
-                utils::interpolate_profile(&member_profile.speed_profile, data.temp.unwrap())
+            .filter_map(|profile| {
+                let Some(temp_source_device) = self
+                    .all_devices
+                    .get(profile.temp_source.device_uid.as_str())
+                else {
+                    error!(
+                        "Temperature Source Device is currently not present: {}",
+                        data.profile.temp_source.device_uid
+                    );
+                    return None;
+                };
+
+                Some((profile.speed_profile, temp_source_device))
             })
-            .collect();
+            .collect::<Vec<(Vec<(f64, u8)>, &Arc<RwLock<Device>>)>>();
+
+        let mut member_duties = Vec::new();
+        for (speed_profile, device) in temp_source_devices {
+            let Some(temp) = device
+                .read()
+                .await
+                .status_history
+                .iter()
+                .last() // last = latest temp
+                .and_then(|status| {
+                    status
+                        .temps
+                        .iter()
+                        .filter(|temp_status| {
+                            temp_status.name == data.profile.temp_source.temp_name
+                        })
+                        .map(|temp_status| temp_status.temp)
+                        .last()
+                })
+            else {
+                todo!()
+            };
+
+            let duty = utils::interpolate_profile(&speed_profile, temp);
+            member_duties.push(duty);
+        }
 
         match data.profile.function.f_type {
             FunctionType::Min => data.duty = member_duties.iter().min().copied(),
@@ -92,4 +144,3 @@ impl Processor for MixProfileProcessor {
 //mod tests {
 //    fn mix_profile_max
 //}
-
