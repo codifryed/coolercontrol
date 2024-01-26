0dc82ec3f8e37c6f010c3994e9e9b571a0e6ce12
Guy Boldon <gb@guyboldon.com>
2024-01-27 00:29:15 +0100

    daemon: add reset password cli option

--- coolercontrold/src/main.rs
+++ coolercontrold/src/main.rs
@@ -85,10 +85,14 @@ struct Args {
     #[clap(long, short)]
     backup: bool,
 
-    /// Migrate your fan curve setting to Profiles introduced in v0.18.0. This will also
+    /// Migrate your fan curve setting to Profiles introduced in v1.0.0. This will also
     /// backup your current settings
     #[clap(long)]
     migrate: bool,
+
+    /// Reset the UI password to the default
+    #[clap(long)]
+    reset_password: bool,
 }
 
 /// Main Control Loop
@@ -141,6 +145,9 @@ async fn main() -> Result<()> {
         }
         info!("Total Profiles migrated: {migrated_profile_count}");
         return config.save_config_file().await;
+    } else if cmd_args.reset_password {
+        info!("Resetting UI password to default");
+        return admin::reset_passwd().await;
     }
     config.save_config_file().await?; // verifies write-ability
     admin::load_passwd().await?;
