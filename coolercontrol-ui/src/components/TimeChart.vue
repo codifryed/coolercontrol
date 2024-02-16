e602d61d8472c8a4436a27520f39161bb5b9460a
Guy Boldon <gb@guyboldon.com>
2024-02-16 19:52:35 +0100

    ui: adjust rpm increments dynamically

--- coolercontrol-ui/src/components/TimeChart.vue
+++ coolercontrol-ui/src/components/TimeChart.vue
@@ -354,20 +354,8 @@ const uOptions: uPlot.Options = {
             stroke: colors.themeColors().text_title,
             size: deviceStore.getREMSize(2.5),
             font: `${deviceStore.getREMSize(1)}px sans-serif`,
-            incrs: [50, 200, 1_000],
-            // values: (_, ticks) => ticks.map((rawValue) => rawValue + ' rpm'),
-            // gap: 3,
-            // ticks: {
-            //     show: true,
-            //     stroke: colors.themeColors().text_title,
-            //     width: 1,
-            //     size: 5,
-            // },
-            // border: {
-            //     show: true,
-            //     width: 1,
-            //     stroke: colors.themeColors().text_title,
-            // },
+            incrs: (_self: uPlot, _axisIdx: number, _scaleMin: number, scaleMax: number) =>
+                scaleMax < 600 ? [50, 200, 1_000] : [200, 1000],
             grid: {
                 show: false,
             },
