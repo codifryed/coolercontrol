3290b27f0ae87e637207fb9bc45e170ebded3bfd
Guy Boldon <gb@guyboldon.com>
2024-03-02 22:19:53 +0100

    ui: enable cli options for tauri app

--- coolercontrol-ui/src-tauri/src/main.rs
+++ coolercontrol-ui/src-tauri/src/main.rs
@@ -146,6 +146,32 @@ fn main() {
             set_active_mode,
         ])
         .setup(|app| {
+            match app.get_cli_matches() {
+                Ok(matches) => {
+                    if matches.args.get("help").is_some() {
+                        println!(
+                            "
+CoolerControl GUI Desktop Application v{}
+
+OPTIONS:
+-h, --help       Print help information (this)
+-v, --version    Print version information",
+                            app.package_info().version
+                        );
+                        std::process::exit(0);
+                    } else if matches.args.get("version").is_some()
+                        && matches.args.get("version").unwrap().value.is_null()
+                    {
+                        // value is Bool(false) if no argument is given...
+                        println!(
+                            "CoolerControl GUI Desktop Application v{}",
+                            app.package_info().version
+                        );
+                        std::process::exit(0);
+                    }
+                }
+                Err(_) => {}
+            }
             let mut store = StoreBuilder::new(app.handle(), CONFIG_FILE.parse()?).build();
             let _ = store.load();
             let start_in_tray = store
