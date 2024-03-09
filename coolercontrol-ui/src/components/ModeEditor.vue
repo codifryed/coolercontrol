cd73c844d0173bb5a2ea370cec573f66b01fbb5b
Guy Boldon <gb@guyboldon.com>
2024-03-09 17:23:30 +0100

    ci: formatting

--- coolercontrol-ui/src/components/ModeEditor.vue
+++ coolercontrol-ui/src/components/ModeEditor.vue
@@ -84,12 +84,13 @@ const initTableData = () => {
             const channelModeSetting = modeSettings.get(device.uid)?.get(channelName)
             let info: string = 'Unknown'
             if (channelInfo.speed_options != null) {
-                if (channelModeSetting == null) { // This means there doesn't exist a setting for this channel.
+                if (channelModeSetting == null) {
+                    // This means there doesn't exist a setting for this channel.
                     continue
                     // info = 'Default Profile'
-                    // Displaying this as a Default Profile is an issue if one mode has a 
+                    // Displaying this as a Default Profile is an issue if one mode has a
                     // setting for a channel and another mode doesn't. Then switching won't set
-                    //  it back to 'default'. By not displaying the setting, as least we are 
+                    //  it back to 'default'. By not displaying the setting, as least we are
                     // indicating to the user that there is no setting for this channel.
                 } else if (channelModeSetting.speed_fixed != null) {
                     info = `Manual: ${channelModeSetting.speed_fixed}%`
