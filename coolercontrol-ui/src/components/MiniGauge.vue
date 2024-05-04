9b76491236f5353fd1d00d03dcdf34209ad073b6
Guy Boldon <gb@guyboldon.com>
2024-05-04 21:19:17 +0200

    ui: improve element scaling

--- coolercontrol-ui/src/components/MiniGauge.vue
+++ coolercontrol-ui/src/components/MiniGauge.vue
@@ -157,14 +157,14 @@ const option: EChartsOption = {
             max: gaugeMax,
             progress: {
                 show: true,
-                width: 10,
+                width: deviceStore.getREMSize(0.625),
                 itemStyle: {
                     color: getSensorColor(),
                 },
             },
             axisLine: {
                 lineStyle: {
-                    width: 10,
+                    width: deviceStore.getREMSize(0.625),
                     color: [[1, colors.themeColors.bg_three]],
                 },
             },
@@ -184,14 +184,14 @@ const option: EChartsOption = {
                 // icon: 'triangle',
                 icon: 'path://M2090.36389,615.30999 L2090.36389,615.30999 C2091.48372,615.30999 2092.40383,616.194028 2092.44859,617.312956 L2096.90698,728.755929 C2097.05155,732.369577 2094.2393,735.416212 2090.62566,735.56078 C2090.53845,735.564269 2090.45117,735.566014 2090.36389,735.566014 L2090.36389,735.566014 C2086.74736,735.566014 2083.81557,732.63423 2083.81557,729.017692 C2083.81557,728.930412 2083.81732,728.84314 2083.82081,728.755929 L2088.2792,617.312956 C2088.32396,616.194028 2089.24407,615.30999 2090.36389,615.30999 Z',
                 length: '90%',
-                width: 3,
+                width: deviceStore.getREMSize(0.1875),
                 itemStyle: {
                     color: colors.themeColors.context_color,
                 },
             },
             anchor: {
                 show: true,
-                size: 6,
+                size: deviceStore.getREMSize(0.375),
                 itemStyle: {
                     borderWidth: 1,
                     borderColor: colors.themeColors.context_hover,
