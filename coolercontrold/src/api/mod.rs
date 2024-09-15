87a9b5438fcdb71331b06b2f3d11c21008967057
Guy Boldon <gb@guyboldon.com>
2024-09-15 11:38:43 +0200

    feat: compress setting for conditional compress of api responses

--- coolercontrold/src/api/mod.rs
+++ coolercontrold/src/api/mod.rs
@@ -28,7 +28,7 @@ use actix_session::{Session, SessionMiddleware};
 use actix_web::dev::{RequestHead, Server};
 use actix_web::http::header::{HeaderValue, AUTHORIZATION};
 use actix_web::http::StatusCode;
-use actix_web::middleware::{Compat, Condition, Logger, NormalizePath};
+use actix_web::middleware::{Compat, Compress, Condition, Logger, NormalizePath};
 use actix_web::web::{Data, Json};
 use actix_web::{
     cookie, get, post, put, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
@@ -526,6 +526,7 @@ pub async fn init_server(
             "Could not bind API to any address. No API and UI connection available."
         ));
     }
+    let compression_enabled = config.get_settings().await?.compress;
     let ipv4 = ipv4_result.ok();
     let ipv6 = ipv6_result.ok();
     let move_all_devices = all_devices.clone();
@@ -548,6 +549,7 @@ pub async fn init_server(
             )
             .wrap(config_cors(ipv4, ipv6))
             .wrap(NormalizePath::trim()) // removes trailing slashes for more flexibility
+            .wrap(Condition::new(compression_enabled, Compress::default()))
             .configure(|cfg| {
                 config_server(
                     cfg,
