3e588f9ec0ef4c6794d2b5e1b73dc02b4b92eb30
Guy Boldon <gb@guyboldon.com>
2024-09-24 13:35:00 +0200

    chore: set toast styling

--- coolercontrol-ui/src/presets/cc/toast/index.js
+++ coolercontrol-ui/src/presets/cc/toast/index.js
@@ -2,39 +2,39 @@ export default {
     root: ({ props }) => ({
         class: [
             //Size and Shape
-            'w-96 rounded-md',
+            'w-96 rounded-lg',
 
             // Positioning
             {
                 '-translate-x-2/4':
-                    props.position == 'top-center' || props.position == 'bottom-center',
+                    props.position === 'top-center' || props.position === 'bottom-center',
             },
         ],
     }),
     message: ({ props }) => ({
         class: [
-            'my-4 rounded-md w-full',
-            'border-solid border-0 border-l-[6px]',
-            'backdrop-blur-[10px] shadow-md',
+            'my-4 rounded-lg w-full',
+            'border-0 border-l-[6px]',
+            'backdrop-blur-[10px] shadow-lg',
 
             // Colors
             {
-                'bg-blue-100/70 dark:bg-blue-500/20': props.message.severity == 'info',
-                'bg-green-100/70 dark:bg-green-500/20': props.message.severity == 'success',
-                'bg-orange-100/70 dark:bg-orange-500/20': props.message.severity == 'warn',
-                'bg-red-100/70 dark:bg-red-500/20': props.message.severity == 'error',
+                'bg-bg-two/50': props.message.severity === 'info' ||
+                    props.message.severity === 'success' ||
+                    props.message.severity === 'warn' ||
+                    props.message.severity === 'error',
             },
             {
-                'border-blue-500 dark:border-blue-400': props.message.severity == 'info',
-                'border-green-500 dark:border-green-400': props.message.severity == 'success',
-                'border-orange-500 dark:border-orange-400': props.message.severity == 'warn',
-                'border-red-500 dark:border-red-400': props.message.severity == 'error',
+                'border-accent': props.message.severity === 'info',
+                'border-green': props.message.severity === 'success',
+                'border-yellow': props.message.severity === 'warn',
+                'border-red': props.message.severity === 'error',
             },
             {
-                'text-blue-700 dark:text-blue-300': props.message.severity == 'info',
-                'text-green-700 dark:text-green-300': props.message.severity == 'success',
-                'text-orange-700 dark:text-orange-300': props.message.severity == 'warn',
-                'text-red-700 dark:text-red-300': props.message.severity == 'error',
+                'text-text-color': props.message.severity === 'info' ||
+                    props.message.severity === 'success',
+                'text-yellow': props.message.severity === 'warn',
+                'text-red': props.message.severity === 'error',
             },
         ],
     }),
@@ -47,13 +47,21 @@ export default {
             },
         ],
     }),
-    messageIcon: {
+    messageIcon: ({ props }) => ({
         class: [
             // Sizing and Spacing
             'w-6 h-6',
             'text-lg leading-none mr-2 shrink-0',
+
+            // Colors
+            {
+                'text-accent': props.message.severity === 'info',
+                'text-green': props.message.severity === 'success',
+                'text-yellow': props.message.severity === 'warn',
+                'text-red': props.message.severity === 'error',
+            },
         ],
-    },
+    }),
     messageText: {
         class: [
             // Font and Text
@@ -83,13 +91,13 @@ export default {
             'rounded-full',
 
             // Colors
-            'bg-transparent',
+            'bg-transparent outline-0',
 
             // Transitions
             'transition duration-200 ease-in-out',
 
             // States
-            'hover:bg-surface-0/50 dark:hover:bg-surface-0/10',
+            'hover:bg-surface-hover/50',
 
             // Misc
             'overflow-hidden',
