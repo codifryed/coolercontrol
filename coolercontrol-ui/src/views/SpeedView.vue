b352bc31b223379377745e844be8da6c2453dfe5
Guy Boldon <gb@guyboldon.com>
2025-02-01 14:11:49 +0100

    feat: edit Profile button for control channels
    a little better flow for changing settings

--- coolercontrol-ui/src/views/SpeedView.vue
+++ coolercontrol-ui/src/views/SpeedView.vue
@@ -19,7 +19,7 @@
 <script setup lang="ts">
 // @ts-ignore
 import SvgIcon from '@jamescoyle/vue-icon'
-import { mdiAlertOutline, mdiContentSaveOutline } from '@mdi/js'
+import { mdiAlertOutline, mdiContentSaveOutline, mdiPencilOutline } from '@mdi/js'
 import Select from 'primevue/select'
 import { nextTick, onMounted, ref, type Ref, watch } from 'vue'
 import { Profile, ProfileType } from '@/models/Profile'
@@ -46,7 +46,7 @@ import SensorTable from '@/components/SensorTable.vue'
 import AxisOptions from '@/components/AxisOptions.vue'
 import { v4 as uuidV4 } from 'uuid'
 import _ from 'lodash'
-import { onBeforeRouteLeave, onBeforeRouteUpdate } from 'vue-router'
+import { onBeforeRouteLeave, onBeforeRouteUpdate, useRouter } from 'vue-router'
 import { useConfirm } from 'primevue/useconfirm'
 
 interface Props {
@@ -61,6 +61,7 @@ const deviceStore = useDeviceStore()
 const { currentDeviceStatus } = storeToRefs(deviceStore)
 const componentKey: Ref<number> = ref(0)
 const confirm = useConfirm()
+const router = useRouter()
 
 let contextIsDirty: boolean = false
 
@@ -305,19 +306,42 @@ onMounted(() => {
             </div>
             <div
                 v-else-if="chosenViewType === ChannelViewType.Control && !manualControlEnabled"
-                class="p-2 pr-0"
+                class="flex flex-row"
             >
-                <Select
-                    v-model="selectedProfile"
-                    :options="getProfileOptions()"
-                    option-label="name"
-                    placeholder="Profile"
-                    class="w-full mr-4 h-full"
-                    checkmark
-                    dropdown-icon="pi pi-chart-line"
-                    scroll-height="40rem"
-                    v-tooltip.bottom="'Profile to apply'"
-                />
+                <div class="p-2 pr-1">
+                    <Button
+                        class="!p-2 w-10 h-[2.375rem]"
+                        label="Edit"
+                        v-tooltip.bottom="'Edit Profile'"
+                        @click="
+                            router.push({
+                                name: 'profiles',
+                                params: { profileUID: selectedProfile.uid },
+                            })
+                        "
+                        :disabled="selectedProfile.uid === '0'"
+                    >
+                        <svg-icon
+                            class="outline-0"
+                            type="mdi"
+                            :path="mdiPencilOutline"
+                            :size="deviceStore.getREMSize(1.5)"
+                        />
+                    </Button>
+                </div>
+                <div class="p-2 pr-0">
+                    <Select
+                        v-model="selectedProfile"
+                        :options="getProfileOptions()"
+                        option-label="name"
+                        placeholder="Profile"
+                        class="w-full mr-4 h-full"
+                        checkmark
+                        dropdown-icon="pi pi-chart-line"
+                        scroll-height="40rem"
+                        v-tooltip.bottom="'Profile to apply'"
+                    />
+                </div>
             </div>
             <div
                 v-else-if="
