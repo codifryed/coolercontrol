f212d0769bdfbaa016f6048eaa81374855fda85b
Guy Boldon <gb@guyboldon.com>
2025-06-22 17:33:12 +0200

    fix: improve thinkpad handling and place in own submodule

--- coolercontrold/src/repositories/hwmon/fans.rs
+++ coolercontrold/src/repositories/hwmon/fans.rs
@@ -513,16 +513,7 @@ pub async fn set_pwm_enable(
         ));
     }
     let path_pwm_enable = base_path.join(format_pwm_enable!(channel_info.number));
-    cc_fs::write_string(&path_pwm_enable, pwm_enable_value.to_string())
-        .await
-        .map_err(|err| {
-            anyhow!(
-                "Unable to set pwm_enable for {path_pwm_enable:?} to {pwm_enable_value}. \
-                    Most likely because of a limitation set by the driver or a BIOS setting; \
-                    Error: {err}"
-            )
-        })?;
-    Ok(())
+    write_pwm_enable(&path_pwm_enable, pwm_enable_value).await
 }
 
 /// This sets `pwm_enable` to the desired value if it's not already set to the desired value.
@@ -543,40 +534,27 @@ pub async fn set_pwm_enable_if_not_already(
     if current_pwm_enable == pwm_enable_value {
         Ok(())
     } else {
-        cc_fs::write_string(&path_pwm_enable, pwm_enable_value.to_string())
-            .await
-            .map_err(|err| {
-                anyhow!(
-                    "Unable to set pwm_enable for {path_pwm_enable:?} to {pwm_enable_value}. \
-                    Most likely because of a limitation set by the driver or a BIOS setting; \
-                    Error: {err}"
-                )
-            })
+        write_pwm_enable(&path_pwm_enable, pwm_enable_value).await
     }
 }
 
-/// This sets `pwm_enable` to 0. The effect of this is dependent on the device, but is primarily used
-/// for `ThinkPads` where this means "full-speed". See:
-/// [Kernel Doc](https://www.kernel.org/doc/html/latest/admin-guide/laptops/thinkpad-acpi.html#fan-control-and-monitoring-fan-speed-fan-enable-disable)
-pub async fn set_thinkpad_to_full_speed(
-    base_path: &Path,
-    channel_info: &HwmonChannelInfo,
-) -> Result<()> {
-    let path_pwm_enable = base_path.join(format_pwm_enable!(channel_info.number));
-    let current_pwm_enable = cc_fs::read_sysfs(&path_pwm_enable)
+async fn write_pwm_enable(path_pwm_enable: &Path, pwm_enable_value: u8) -> Result<()> {
+    cc_fs::write_string(&path_pwm_enable, pwm_enable_value.to_string())
         .await
-        .and_then(check_parsing_8)?;
-    if current_pwm_enable != PWM_ENABLE_THINKPAD_FULL_SPEED {
-        cc_fs::write_string(&path_pwm_enable, PWM_ENABLE_THINKPAD_FULL_SPEED.to_string())
-            .await
-            .map_err(|err| {
-                anyhow!(
-                    "Not able to set pwm_enable to 0. Most likely because of a permissions \
-                    issue or driver limitation; Error: {err}"
-                )
-            })?;
-    }
-    Ok(())
+        .inspect(|()| {
+            debug!(
+                "Applied pwm_enable for {} of {pwm_enable_value}",
+                path_pwm_enable.display()
+            );
+        })
+        .map_err(|err| {
+            anyhow!(
+                "Unable to set pwm_enable for {} to {pwm_enable_value}. \
+                    Most likely because of a limitation set by the driver or a BIOS setting; \
+                    Error: {err}",
+                path_pwm_enable.display()
+            )
+        })
 }
 
 pub async fn set_pwm_duty(
@@ -609,14 +587,89 @@ fn duty_to_pwm_value(speed_duty: u8) -> u8 {
     ((clamped_duty * 25.5).round() / 10.0).round() as u8
 }
 
+/// submodule for `ThinkPad` fan logic
+pub mod thinkpad {
+    use crate::cc_fs;
+    use crate::config::Config;
+    use crate::repositories::hwmon::fans::{
+        check_parsing_8, set_pwm_duty, set_pwm_enable_if_not_already, PWM_ENABLE_MANUAL_VALUE,
+        PWM_ENABLE_THINKPAD_FULL_SPEED,
+    };
+    use crate::repositories::hwmon::hwmon_repo::{HwmonChannelInfo, HwmonDriverInfo};
+    use anyhow::{anyhow, Result};
+    use log::debug;
+    use std::path::Path;
+    use std::rc::Rc;
+
+    pub async fn apply_speed_fixed(
+        config: &Rc<Config>,
+        hwmon_driver: &Rc<HwmonDriverInfo>,
+        channel_info: &HwmonChannelInfo,
+        speed_fixed: u8,
+    ) -> Result<()> {
+        if speed_fixed == 100 && config.get_settings()?.thinkpad_full_speed {
+            set_to_full_speed(&hwmon_driver.path, channel_info).await
+        } else {
+            set_pwm_enable_if_not_already(
+                PWM_ENABLE_MANUAL_VALUE,
+                &hwmon_driver.path,
+                channel_info,
+            )
+            .await?;
+            set_pwm_duty(&hwmon_driver.path, channel_info, speed_fixed)
+                .await
+                .map_err(|err| {
+                    anyhow!(
+                        "Error on {}:{} for duty {speed_fixed} - {err}",
+                        hwmon_driver.name,
+                        channel_info.name
+                    )
+                })
+        }
+    }
+
+    /// This sets `pwm_enable` to 0. The effect of this is dependent on the device, but is primarily used
+    /// for `ThinkPads` where this means "full-speed". See:
+    /// [Kernel Doc](https://www.kernel.org/doc/html/latest/admin-guide/laptops/thinkpad-acpi.html#fan-control-and-monitoring-fan-speed-fan-enable-disable)
+    pub async fn set_to_full_speed(
+        base_path: &Path,
+        channel_info: &HwmonChannelInfo,
+    ) -> Result<()> {
+        // set to 100% first for consistent pwm duty-reporting behavior
+        // (the driver doesn't automatically set the duty to 100% in full-speed mode)
+        set_pwm_duty(base_path, channel_info, 100).await?;
+        let path_pwm_enable = base_path.join(format_pwm_enable!(channel_info.number));
+        let current_pwm_enable = cc_fs::read_sysfs(&path_pwm_enable)
+            .await
+            .and_then(check_parsing_8)?;
+        if current_pwm_enable != PWM_ENABLE_THINKPAD_FULL_SPEED {
+            cc_fs::write_string(&path_pwm_enable, PWM_ENABLE_THINKPAD_FULL_SPEED.to_string())
+                .await
+                .inspect(|()| {
+                    debug!("Applied pwm_enable for {} of {PWM_ENABLE_THINKPAD_FULL_SPEED}", path_pwm_enable.display());
+                })
+                .map_err(|err| {
+                    anyhow!(
+                        "Not able to set pwm_enable of {PWM_ENABLE_THINKPAD_FULL_SPEED}. \
+                        Most likely because of a permissions issue or driver limitation; Error: {err}"
+                    )
+                })?;
+        }
+        Ok(())
+    }
+}
+
 /// Tests
 #[cfg(test)]
+#[allow(clippy::float_cmp)]
 mod tests {
+    use super::*;
+    use crate::config::Config;
+    use crate::setting::CoolerControlSettings;
     use serial_test::serial;
     use std::path::Path;
+    use std::rc::Rc;
     use uuid::Uuid;
-
-    use super::*;
 
     const TEST_BASE_PATH_STR: &str = "/tmp/coolercontrol-tests-";
 
@@ -1055,6 +1108,226 @@ mod tests {
             teardown(&ctx);
             assert!(result.is_ok());
             assert_eq!(current_duty.to_string(), "50");
+        });
+    }
+
+    #[test]
+    #[serial]
+    fn thinkpad_apply_speed() {
+        cc_fs::test_runtime(async {
+            let ctx = setup();
+            // given:
+            let test_base_path = &ctx.test_base_path;
+            cc_fs::write(
+                test_base_path.join("pwm1_enable"),
+                PWM_ENABLE_MANUAL_VALUE.to_string().into_bytes(),
+            )
+            .await
+            .unwrap();
+            cc_fs::write(test_base_path.join("pwm1"), b"0".to_vec())
+                .await
+                .unwrap();
+            let channel_info = HwmonChannelInfo {
+                hwmon_type: HwmonChannelType::Fan,
+                number: 1,
+                pwm_enable_default: Some(2),
+                name: String::new(),
+                label: None,
+                pwm_mode_supported: false,
+                pwm_writable: true,
+            };
+            let config = Rc::new(Config::init_default_config().unwrap());
+            // set full_speed setting
+            let cc_settings = CoolerControlSettings {
+                thinkpad_full_speed: true,
+                ..Default::default()
+            };
+            config.set_settings(&cc_settings);
+            let hwmon_info = Rc::new(HwmonDriverInfo {
+                path: test_base_path.clone(),
+                ..Default::default()
+            });
+
+            // when:
+            let result = thinkpad::apply_speed_fixed(&config, &hwmon_info, &channel_info, 50).await;
+
+            // then:
+            let current_pwm_enable = cc_fs::read_sysfs(&test_base_path.join("pwm1_enable"))
+                .await
+                .unwrap();
+            let current_duty = cc_fs::read_sysfs(&test_base_path.join("pwm1"))
+                .await
+                .and_then(check_parsing_8)
+                .map(pwm_value_to_duty)
+                .unwrap();
+            teardown(&ctx);
+            assert!(result.is_ok());
+            assert_eq!(current_pwm_enable, PWM_ENABLE_MANUAL_VALUE.to_string());
+            assert_eq!(current_duty, 50.);
+        });
+    }
+
+    #[test]
+    #[serial]
+    fn thinkpad_apply_speed_full_speed() {
+        cc_fs::test_runtime(async {
+            let ctx = setup();
+            // given:
+            let test_base_path = &ctx.test_base_path;
+            cc_fs::write(
+                test_base_path.join("pwm1_enable"),
+                PWM_ENABLE_MANUAL_VALUE.to_string().into_bytes(),
+            )
+            .await
+            .unwrap();
+            cc_fs::write(test_base_path.join("pwm1"), b"0".to_vec())
+                .await
+                .unwrap();
+            let channel_info = HwmonChannelInfo {
+                hwmon_type: HwmonChannelType::Fan,
+                number: 1,
+                pwm_enable_default: Some(2),
+                name: String::new(),
+                label: None,
+                pwm_mode_supported: false,
+                pwm_writable: true,
+            };
+            let config = Rc::new(Config::init_default_config().unwrap());
+            // set full_speed setting
+            let cc_settings = CoolerControlSettings {
+                thinkpad_full_speed: true,
+                ..Default::default()
+            };
+            config.set_settings(&cc_settings);
+            let hwmon_info = Rc::new(HwmonDriverInfo {
+                path: test_base_path.clone(),
+                ..Default::default()
+            });
+
+            // when:
+            let result =
+                thinkpad::apply_speed_fixed(&config, &hwmon_info, &channel_info, 100).await;
+
+            // then:
+            let current_pwm_enable = cc_fs::read_sysfs(&test_base_path.join("pwm1_enable"))
+                .await
+                .unwrap();
+            let current_duty = cc_fs::read_sysfs(&test_base_path.join("pwm1"))
+                .await
+                .and_then(check_parsing_8)
+                .map(pwm_value_to_duty)
+                .unwrap();
+            teardown(&ctx);
+            assert!(result.is_ok());
+            assert_eq!(
+                current_pwm_enable,
+                PWM_ENABLE_THINKPAD_FULL_SPEED.to_string()
+            );
+            assert_eq!(current_duty, 100.);
+        });
+    }
+
+    #[test]
+    #[serial]
+    fn thinkpad_apply_speed_after_full_speed() {
+        cc_fs::test_runtime(async {
+            let ctx = setup();
+            // given:
+            let test_base_path = &ctx.test_base_path;
+            cc_fs::write(
+                test_base_path.join("pwm1_enable"),
+                PWM_ENABLE_THINKPAD_FULL_SPEED.to_string().into_bytes(),
+            )
+            .await
+            .unwrap();
+            cc_fs::write(test_base_path.join("pwm1"), b"255".to_vec())
+                .await
+                .unwrap();
+            let channel_info = HwmonChannelInfo {
+                hwmon_type: HwmonChannelType::Fan,
+                number: 1,
+                pwm_enable_default: Some(2),
+                name: String::new(),
+                label: None,
+                pwm_mode_supported: false,
+                pwm_writable: true,
+            };
+            let config = Rc::new(Config::init_default_config().unwrap());
+            // set full_speed setting
+            let cc_settings = CoolerControlSettings {
+                thinkpad_full_speed: true,
+                ..Default::default()
+            };
+            config.set_settings(&cc_settings);
+            let hwmon_info = Rc::new(HwmonDriverInfo {
+                path: test_base_path.clone(),
+                ..Default::default()
+            });
+
+            // when:
+            let result = thinkpad::apply_speed_fixed(&config, &hwmon_info, &channel_info, 50).await;
+
+            // then:
+            let current_pwm_enable = cc_fs::read_sysfs(&test_base_path.join("pwm1_enable"))
+                .await
+                .unwrap();
+            let current_duty = cc_fs::read_sysfs(&test_base_path.join("pwm1"))
+                .await
+                .and_then(check_parsing_8)
+                .map(pwm_value_to_duty)
+                .unwrap();
+            teardown(&ctx);
+            assert!(result.is_ok());
+            assert_eq!(current_pwm_enable, PWM_ENABLE_MANUAL_VALUE.to_string(),);
+            assert_eq!(current_duty, 50.);
+        });
+    }
+
+    #[test]
+    #[serial]
+    fn thinkpad_set_full_speed() {
+        cc_fs::test_runtime(async {
+            let ctx = setup();
+            // given:
+            let test_base_path = &ctx.test_base_path;
+            cc_fs::write(
+                test_base_path.join("pwm1_enable"),
+                PWM_ENABLE_MANUAL_VALUE.to_string().into_bytes(),
+            )
+            .await
+            .unwrap();
+            cc_fs::write(test_base_path.join("pwm1"), b"0".to_vec())
+                .await
+                .unwrap();
+            let channel_info = HwmonChannelInfo {
+                hwmon_type: HwmonChannelType::Fan,
+                number: 1,
+                pwm_enable_default: Some(2),
+                name: String::new(),
+                label: None,
+                pwm_mode_supported: false,
+                pwm_writable: true,
+            };
+
+            // when:
+            let result = thinkpad::set_to_full_speed(test_base_path, &channel_info).await;
+
+            // then:
+            let current_pwm_enable = cc_fs::read_sysfs(&test_base_path.join("pwm1_enable"))
+                .await
+                .unwrap();
+            let current_duty = cc_fs::read_sysfs(&test_base_path.join("pwm1"))
+                .await
+                .and_then(check_parsing_8)
+                .map(pwm_value_to_duty)
+                .unwrap();
+            teardown(&ctx);
+            assert!(result.is_ok());
+            assert_eq!(
+                current_pwm_enable,
+                PWM_ENABLE_THINKPAD_FULL_SPEED.to_string()
+            );
+            assert_eq!(current_duty, 100.);
         });
     }
 }
