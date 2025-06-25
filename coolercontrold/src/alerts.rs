de466e5d61e733f571925d7caa9dbe2fc0a6d178
Eren <eren.simsek@tuta.io>
2025-06-25 18:49:51 +0100

    feat(deamon): warmup_duration field for alerts to delay activation
    `warmup_duration` field is an f64 representing seconds throughout which the
    alert condition must hold before the alert is activated. During this time the
    alert may go back to inactive state without ever being activated.
    
    `warmup_duration` defaults to 1 for newly created alerts and to 0 for existing
    alerts. This is for backwards compatibility.
    
    Warmup is achieved by a new `AlertState`: `WarmUp(DateTime<Local>)`. `WarmUp`
    holds the datetime at which we entered the warmup state. We keep track of this
    and activate the alert when it's been at least `warmup_duration` seconds since
    we entered `WarmUp`. This means we don't guarantee 100% accuracy in handling
    `WarmUp` -> `Active` transitions and even 0 `warmup_duration` will be in
    `WarmUp` state for a brief period - though this should be invisible to the user
    for the most part so I opted not to handle a special case.
    
    There's another new state that is not directly related to this change: `Error`.
    In the previous implementation, the alert was put in `Active` state when an
    error occured while processing it. That use case is now replaced with `Error`
    which, similar to `Activate`, sends an alert message. This allows to us to treat
    them differently during state change handling as well as allows clients to
    differentiate alert activations from internal errors.
    
    Setting the state is offloaded to a basic state machine in `Alert::set_state`.
    This function handles non-error state changes and assumes the error condition
    must've been resolved if its called. Any state can transition into an `Error`
    and `Error` can transition to `Inactive` or `WarmUp` depending on the condition
    at the moment the error is resolved. Ignoring `Error` below are the possible
    state transitions:
    
    - `Inactive` -> `WarmUp`
    - `WarmUp` -> `Inactive`
    - `WarmUp` -> `Active`
    - `Active` -> `Inactive`
    
    With the introduction of the state machine and the `Error` state,
    `AlertController::activate_alert` and `AlertController::deactivate_alert` are
    replaced with `AlertController::activate_alert_with_error`. This function
    behaves similar to `activate_alert` but sets the state to `Error`. Since the
    non-error state changes are handled elsewhere, the other two functions are no
    longer needed.

--- coolercontrold/src/alerts.rs
+++ coolercontrold/src/alerts.rs
@@ -26,7 +26,6 @@ use anyhow::{Context, Result};
 use chrono::{DateTime, Local};
 use const_format::concatcp;
 use hashlink::LinkedHashMap;
-use lazy_format::lazy_format;
 use log::{error, info, trace};
 use moro_local::Scope;
 use schemars::JsonSchema;
@@ -54,12 +53,67 @@ pub struct Alert {
     pub min: f64,
     pub max: f64,
     pub state: AlertState,
+    /// Time in seconds throughout which the alert conditidon must hold before the alert is
+    /// activated.
+    //
+    // For backwards compatibility, default to 0 to a) tolerate missing fields and b) preserve the
+    // previous behavior. New instances will default to 1 second.
+    #[serde(default)]
+    pub warmup_duration: f64,
+}
+
+impl Alert {
+    /// Updates the state based on [`value`] and returns whether the state changed.
+    fn set_state(&mut self, value: f64) -> bool {
+        if value > self.min && value < self.max {
+            let changed = self.state != AlertState::Inactive;
+            self.state = AlertState::Inactive;
+            return changed;
+        }
+
+        // We know we're out of bounds here.
+        match self.state {
+            AlertState::Active => {}
+            AlertState::WarmUp(time) => {
+                if Local::now().signed_duration_since(time).as_seconds_f64() >= self.warmup_duration
+                {
+                    self.state = AlertState::Active;
+                    return true;
+                }
+            }
+            // Error state means we could not retrieve the channel value. But if we're here with a
+            // channel value it means the errors were resolved e.g. by a daemon restart. Act as
+            // usual.
+            AlertState::Error | AlertState::Inactive => {
+                self.state = AlertState::WarmUp(Local::now());
+                return true;
+            }
+        };
+
+        false
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Display, EnumString, Serialize, Deserialize, JsonSchema)]
 pub enum AlertState {
     Active,
+    /// Alert condition was satisfied at the stored time but the duration threshold has not been
+    /// reached
+    WarmUp(DateTime<Local>),
     Inactive,
+    /// Represents an error state. e.g. when one of the components in the alert isn't found.
+    Error,
+}
+
+impl AlertState {
+    fn sends_message(&self) -> bool {
+        match self {
+            AlertState::Active => true,
+            AlertState::WarmUp(_) => false,
+            AlertState::Inactive => true,
+            AlertState::Error => true,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
@@ -245,12 +299,11 @@ impl AlertController {
     }
 
     /// Collects all Alerts that need firing
-    #[allow(clippy::too_many_lines)]
     fn process_and_collect_alerts_to_fire(&self) -> Vec<(Alert, AlertLogMessage)> {
         let mut alerts_to_fire = Vec::new();
         for alert in self.alerts.borrow_mut().values_mut() {
             let Some(device) = self.all_devices.get(&alert.channel_source.device_uid) else {
-                Self::activate_alert(&mut alerts_to_fire, alert, "Device not found");
+                Self::activate_alert_with_error(&mut alerts_to_fire, alert, "Device not found");
                 continue;
             };
             let most_recent_status = device.borrow().status_current().unwrap();
@@ -260,7 +313,11 @@ impl AlertController {
                     .iter()
                     .find(|temp| temp.name == alert.channel_source.channel_name)
                 else {
-                    Self::activate_alert(&mut alerts_to_fire, alert, "Device Channel not found");
+                    Self::activate_alert_with_error(
+                        &mut alerts_to_fire,
+                        alert,
+                        "Device Channel not found",
+                    );
                     continue;
                 };
                 temp_status.temp
@@ -270,13 +327,17 @@ impl AlertController {
                     .iter()
                     .find(|channel| channel.name == alert.channel_source.channel_name)
                 else {
-                    Self::activate_alert(&mut alerts_to_fire, alert, "Device Channel not found");
+                    Self::activate_alert_with_error(
+                        &mut alerts_to_fire,
+                        alert,
+                        "Device Channel not found",
+                    );
                     continue;
                 };
                 match alert.channel_source.channel_metric {
                     ChannelMetric::Duty => {
                         let Some(duty) = channel_status.duty else {
-                            Self::activate_alert(
+                            Self::activate_alert_with_error(
                                 &mut alerts_to_fire,
                                 alert,
                                 "Device Channel Duty Metric not found",
@@ -287,7 +348,7 @@ impl AlertController {
                     }
                     ChannelMetric::Load => {
                         let Some(load) = channel_status.duty else {
-                            Self::activate_alert(
+                            Self::activate_alert_with_error(
                                 &mut alerts_to_fire,
                                 alert,
                                 "Device Channel Load Metric not found",
@@ -298,7 +359,7 @@ impl AlertController {
                     }
                     ChannelMetric::RPM => {
                         let Some(rpm) = channel_status.rpm else {
-                            Self::activate_alert(
+                            Self::activate_alert_with_error(
                                 &mut alerts_to_fire,
                                 alert,
                                 "Device Channel RPM Metric not found",
@@ -309,7 +370,7 @@ impl AlertController {
                     }
                     ChannelMetric::Freq => {
                         let Some(freq) = channel_status.freq else {
-                            Self::activate_alert(
+                            Self::activate_alert_with_error(
                                 &mut alerts_to_fire,
                                 alert,
                                 "Device Channel Freq Metric not found",
@@ -324,75 +385,49 @@ impl AlertController {
                     }
                 }
             };
-            if channel_value > alert.max {
-                if alert.state == AlertState::Active {
-                    continue;
-                }
-                // round up to clearly display greater than.
-                let channel_value_rounded = (channel_value * 10.).ceil() / 10.;
-                let channel_name = alert.channel_source.channel_name.clone();
-                let max = alert.max;
-                Self::activate_alert(
-                    &mut alerts_to_fire,
-                    alert,
-                    lazy_format!(
-                        "{channel_name}: {channel_value_rounded} is greater than allowed maximum: {max}"
-                    ),
-                );
-            } else if channel_value < alert.min {
-                if alert.state == AlertState::Active {
-                    continue;
-                }
-                // round down to clearly display less than.
-                let channel_value_rounded = (channel_value * 10.).floor() / 10.;
-                let channel_name = alert.channel_source.channel_name.clone();
-                let min = alert.min;
-                Self::activate_alert(
-                    &mut alerts_to_fire,
-                    alert,
-                    lazy_format!(
-                        "{channel_name}: {channel_value_rounded} is less than allowed minimum: {min}"
-                    ),
-                );
-            } else if alert.state != AlertState::Inactive {
-                let channel_value_rounded = (channel_value * 10.).round() / 10.;
-                let channel_name = alert.channel_source.channel_name.clone();
-                let min = alert.min;
-                let max = alert.max;
-                Self::deactivate_alert(
-                    &mut alerts_to_fire,
-                    alert,
-                    format!(
-                    "{channel_name}: {channel_value_rounded} is again within allowed range: {min} - {max}"
-                ),
-                );
+
+            // No message if the state didn't change or the current state does not send messages
+            if !alert.set_state(channel_value) || !alert.state.sends_message() {
+                continue;
             }
+
+            // round up to clearly display greater than.
+            let channel_value_rounded = (channel_value * 10.).round() / 10.;
+            let channel_name = alert.channel_source.channel_name.clone();
+            let min = alert.min;
+            let max = alert.max;
+
+            let message = if channel_value > alert.max {
+                format!(
+                        "{channel_name}: {channel_value_rounded} is greater than allowed maximum: {max}"
+                    )
+            } else if channel_value < alert.min {
+                format!(
+                    "{channel_name}: {channel_value_rounded} is less than allowed minimum: {min}"
+                )
+            } else {
+                format!(
+                    "{channel_name}: {channel_value_rounded} is again within allowed range: {min} - {max}"
+                )
+            };
+
+            alerts_to_fire.push((alert.clone(), message.to_string()));
         }
         alerts_to_fire
     }
 
-    /// Adds an Alert to the list of alerts to fire, if state has changed.
-    fn activate_alert(
+    /// Adds an Alert to the list of alerts to fire with error state, if state has changed.
+    fn activate_alert_with_error(
         alerts_to_fire: &mut Vec<(Alert, AlertLogMessage)>,
         alert: &mut Alert,
         message: impl Display,
     ) {
-        if alert.state == AlertState::Active {
+        if alert.state == AlertState::Error {
             return; // only fire on state change
         }
-        alert.state = AlertState::Active;
-        alerts_to_fire.push((alert.clone(), message.to_string()));
-    }
 
-    /// Adds an Alert to the list of alerts to fire. State change should be checked before calling
-    /// this method.
-    fn deactivate_alert(
-        alerts_to_fire: &mut Vec<(Alert, AlertLogMessage)>,
-        alert: &mut Alert,
-        message: String,
-    ) {
-        alert.state = AlertState::Inactive;
-        alerts_to_fire.push((alert.clone(), message));
+        alert.state = AlertState::Error;
+        alerts_to_fire.push((alert.clone(), message.to_string()));
     }
 
     /// Logs an alert state change to the internal buffer, as well as returning the newly
