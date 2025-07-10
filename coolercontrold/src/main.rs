19daf72c58853467f47d58fe5092fe28526dd921
Guy Boldon <gb@guyboldon.com>
2025-07-10 21:45:12 +0200

    chore: simple warning when liquidctl enabled but not found

--- coolercontrold/src/main.rs
+++ coolercontrold/src/main.rs
@@ -331,7 +331,8 @@ async fn initialize_device_repos(
             repos.liquidctl = Some(repo);
         }
         Err(err) => match err.downcast_ref() {
-            Some(&InitError::Disabled | &InitError::Env { .. }) => info!("{err}"),
+            Some(&InitError::Disabled) => info!("{err}"),
+            Some(&InitError::Env { .. }) => warn!("{err}"),
             _ => warn!("Error initializing LIQUIDCTL Repo: {err}"),
         },
     }
