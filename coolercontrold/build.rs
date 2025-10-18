d8be6ad8d1b5e772e079f47950a9a2239e8280d2
Guy Boldon <gb@guyboldon.com>
2025-10-19 00:32:53 +0200

    feat: proto files for external device services

---
+++ coolercontrold/build.rs
@@ -0,0 +1,31 @@
+/*
+ * CoolerControl - monitor and control your cooling and other devices
+ * Copyright (c) 2021-2025  Guy Boldon, Eren Simsek and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_prost_build::configure()
+        .build_server(false)
+        .build_client(true)
+        .compile_protos(
+            &[
+                "resources/proto/coolercontrol/models/v1/device.proto",
+                "resources/proto/coolercontrol/plugins/device_service/v1/device_service.proto",
+            ],
+            &["resources/proto"],
+        )?;
+    Ok(())
+}
