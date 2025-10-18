13e6f3c0e0a4bf30cdd097a725af76a347ad23e2
Guy Boldon <gb@guyboldon.com>
2025-10-19 01:15:03 +0200

    fix: add protoc arg for older protoc versions
    for older ubuntu packages

--- coolercontrold/build.rs
+++ coolercontrold/build.rs
@@ -20,6 +20,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(false)
         .build_client(true)
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &[
                 "resources/proto/coolercontrol/models/v1/device.proto",
