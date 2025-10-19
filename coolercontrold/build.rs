16e7fa6d0b0d6b4c7ef71b8f1fe8b0bbdb6cfc74
Guy Boldon <gb@guyboldon.com>
2025-10-19 13:09:55 +0200

    chore: add comment for reason for arg

--- coolercontrold/build.rs
+++ coolercontrold/build.rs
@@ -20,6 +20,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(false)
         .build_client(true)
+        // needed for older protoc packages:
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &[
