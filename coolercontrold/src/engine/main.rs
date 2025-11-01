17b4608f15ce7ed38eb94ef3aafc74e97dfd4e20
Guy Boldon <gb@guyboldon.com>
2025-11-01 19:43:34 +0100

    chore: cleanup clippy warnings

--- coolercontrold/src/engine/main.rs
+++ coolercontrold/src/engine/main.rs
@@ -146,12 +146,11 @@ impl Engine {
                 Ok((device_lock, repo))
             } else {
                 Err(anyhow!(
-                    "Repository: {:?} for device is currently not running!",
-                    device_type
+                    "Repository: {device_type:?} for device is currently not running!"
                 ))
             }
         } else {
-            Err(anyhow!("Device Not Found: {}", device_uid))
+            Err(anyhow!("Device Not Found: {device_uid}"))
         }
     }
 
@@ -443,8 +442,7 @@ impl Engine {
             .is_empty();
         if lcd_not_enabled {
             return Err(anyhow!(
-                "LCD Screen modes not enabled for this device: {}",
-                device_uid
+                "LCD Screen modes not enabled for this device: {device_uid}"
             ));
         }
         let result = if lcd_settings.mode == "temp" {
@@ -596,7 +594,7 @@ impl Engine {
                         continue;
                     }
                     let reset_setting = Setting {
-                        channel_name: ch.to_string(),
+                        channel_name: ch.clone(),
                         reset_to_default: Some(true),
                         ..Default::default()
                     };
