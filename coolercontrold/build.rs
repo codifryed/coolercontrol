e2c559cb3096217b424bfae491d850040bfb305f
Guy Boldon <gb@guyboldon.com>
2025-11-23 22:31:11 +0100

    feat: add deps for grpc server

--- coolercontrold/build.rs
+++ coolercontrold/build.rs
@@ -18,7 +18,7 @@
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
-        .build_server(false)
+        .build_server(true)
         .build_client(true)
         // needed for older protoc packages:
         .protoc_arg("--experimental_allow_proto3_optional")
