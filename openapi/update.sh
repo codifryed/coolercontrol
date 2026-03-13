9923ea3f5f0e1908108efd63ac6f918f79ca0281
Guy Boldon <gb@guyboldon.com>
2026-03-13 16:59:01 +0100

    refactor: udpate openapi spec stuff

--- openapi/update.sh
+++ openapi/update.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+echo "Start a daemon using the debug build profile (not the release profile) and press any key to continue..."
+read -r _
+
 wget http://localhost:11987/api.json -O openapi.json
+
+echo "Done. Don't forget to commit and copy the new spec to the website."
